6646ce8b44d36d02046d4cae263d81599bccbf8f
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-03 12:56:07 -0400

    added comments. need to code how to keep squares from being marked more than once

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+/* gameBoard array includes all list tags
+   used in event listen array to only listen for clicks on game board*/
 var gameBoard = document.getElementsByTagName("li");
+var counter = 0; /*counter used to determine which mark*/
 
+/* for loop to listen for events on all squares*/
 for (var i = 0; i < gameBoard.length; i++) {
   gameBoard[i].addEventListener("click", markSquare);
 }
-var counter = 0;
+
+
+/*function to change square based on element id*/
 function markSquare(){
   if (counter % 2 === 0){
     /*console.log("mark this X");*/
@@ -14,13 +20,12 @@ function markSquare(){
     document.getElementById(event.target.id).innerHTML = "O";
     document.getElementById(event.target.id).className = "markO";
   }
-  counter ++;
-
+  counter ++; /*increment counter to alternate marks*/
 }
 
 document.getElementById("resetButton").addEventListener("click", resetBoard);
 function resetBoard(){
-  console.log("clicked reset");
+  /*console.log("clicked reset");*/
   for (var i = 0; i < gameBoard.length; i++){
     gameBoard[i].className = "emptySquare";
     gameBoard[i].innerHTML = "";
