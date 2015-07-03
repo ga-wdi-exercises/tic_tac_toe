e8170988f81b4c3a815cbb51f4264c5c4e726920
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-03 09:54:56 -0400

    committing all files. reread instructions, can eliminate some steps from code.

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-/*document.getElementById("rowOneLeft").addEventListener("click", changeMarker);
+document.getElementById("rowOneLeft").addEventListener("click", numberOfClicks);
 document.getElementById("rowOneMiddle").addEventListener("click", changeMarker);
-document.getElementById("rowOneRight").addEventListener("click", changeMarker);*/
+document.getElementById("rowOneRight").addEventListener("click", changeMarker);
+document.getElementById("rowTwoLeft").addEventListener("click", changeMarker);
+document.getElementById("rowTwoMiddle").addEventListener("click", changeMarker);
+document.getElementById("rowTwoRight").addEventListener("click", changeMarker);
+document.getElementById("rowThreeLeft").addEventListener("click", changeMarker);
+document.getElementById("rowThreeMiddle").addEventListener("click", changeMarker);
+document.getElementById("rowThreeRight").addEventListener("click", changeMarker);
 
+/* the following code works but the counter is incorrect
+anywhere on the board i clicked, the counter would increase and not reset
+based on which square was clicked.
+document.addEventListener("click", changeMarker);
 var numberOfClicks = 0;
- function changeMarker(clickedSquare) {
+ function changeMarker() {
    numberOfClicks = numberOfClicks + 1;
    if (numberOfClicks % 3 === 1){
-     document.getElementById("rowOneLeft").innerHTML = "X";
+     document.getElementById(event.target.id).innerHTML = "X";
    } else if (numberOfClicks % 3 === 2) {
-     document.getElementById("rowOneLeft").innerHTML = "O";
+     document.getElementById(event.target.id).innerHTML = "O";
    } else {
-     document.getElementById("rowOneLeft").innerHTML = "";
+     document.getElementById(event.target.id).innerHTML = "";
    }
    return false;
- }
+ }*/
 
-document.addEventListener("click", determineClickedSquare);
-function determineClickedSquare(){
+/*document.addEventListener("click", changeMarker);*/
+function changeMarker() {
+
+  document.getElementById(event.target.id).innerHTML = numberOfClicks();
+}
+
+var clicks = 0;
+function numberOfClicks(){
+  return clicks ++;
+  }
+
+
+
+/*function determineClickedSquare(){
   var clickedSquare = event.target.id;
   console.log(clickedSquare);
-}
+  changeMarker();
+}*/
