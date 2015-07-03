e43c8f3ecacb2dd99e3ec960771db0106d63eba3
Edward Park <parkes.edward@gmail.com>
2015-07-03 17:20:16 -0400

    added floats to create css board and MVP user stories

---
+++ script.js
@@ -0,0 +1,5 @@
+//v.0.1
+
+//as a user I want to click on the box and have it alernate between X and o
+//as a user I want to click all the boxes and have it say draw
+//as a user I want to reset the gameboard
