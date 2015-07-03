66dd81a5a356bea62de78369770ea491ef3ddd44
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-03 11:56:38 -0400

    added reset button. need to redo initial event listener to only include board

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ document.addEventListener("click", markSquare);
 var counter = 0;
 function markSquare(){
   if (counter % 2 === 0){
-    console.log("mark this X");
+    /*console.log("mark this X");*/
     document.getElementById(event.target.id).innerHTML = "X";
     document.getElementById(event.target.id).className = "markX";
   } else {
-    console.log("mark this O");
+    /*console.log("mark this O");*/
     document.getElementById(event.target.id).innerHTML = "O";
     document.getElementById(event.target.id).className = "markO";
   }
   counter ++;
+}
+
+document.getElementById("resetButton").addEventListener("click", resetBoard);
+function resetBoard(){
+  document.getElementsByClassName("mark0").className = "emptySquare";
+  document.getElementsByClassName("markX").className = "emptySquare";
 }
