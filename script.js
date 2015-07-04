9932919e12d60cde58b736bc5a87a49a3a6585ec
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-07-03 22:10:51 -0400

    added functions turnO and turnX

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 //Tic-Tac-Toe game//
 //Access the p element inside my cells
 //Change to "X"
+var turnX = function(){
+  function changeToX(e) {
+    e.target.innerHTML = "<p>X</p>"
+  }
 
-function changeToX(e) {
-  //document.getElementsByClassName("cell")[0].innerHTML = "<p>X</p>";
-  e.target.innerHTML = "<p>X</p>"
+  function position(){
+
+  }
+
+  var allCells = document.getElementsByClassName("cell");
+  for (var i = 0; i < allCells.length; i++){
+    var cellContent = document.getElementsByClassName("cell")[i];
+    cellContent.addEventListener("click", changeToX);
+  }
 }
 
-function position(){
+//Change to O
+var turnO = function() {
+  function changeToO(e) {
+    e.target.innerHTML = "<p>O</p>"
+  }
 
-}
+  function position(){
 
-var allCells = document.getElementsByClassName("cell");
-for (var i = 0; i < allCells.length; i++){
-  var cellContent = document.getElementsByClassName("cell")[i];
-  cellContent.addEventListener("click", changeToX);
+  }
+
+  var allCells = document.getElementsByClassName("cell");
+  for (var i = 0; i < allCells.length; i++){
+    var cellContent = document.getElementsByClassName("cell")[i];
+    cellContent.addEventListener("click", changeToO);
+  }
 }
+turnX();
+turnO();
