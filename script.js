7d7ac84cccf7dfee8c2b694ac7cad742cb3bcc23
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-07-04 17:22:59 -0400

    took away multiple classes from individual cells because I could not access the needed documents through DOM elements. Not sure of the exact reason

--- script.js
+++ script.js
@@ -2,58 +2,25 @@
 // var button = document.getElementsByTagName("button")[0];
 // var buttonText = document.getElementsByClassName("turnSwitchBtn")[0].innerHTML;
 
-//Access the p element inside my cells
-//Change to "X"
-var turnX = function(){
-  function changeToX(e) {
-    e.target.innerHTML = "X"; //whatever div was clicked, add X
-  }
+var allCells = document.getElementsByTagName("div");
+var counter = 0;
+
 //Add click event listener to all cells
-  var allCells = document.getElementsByClassName("cell");
-  for (var i = 0; i < allCells.length; i++){
-    var cellContent = document.getElementsByClassName("cell")[i];
-    cellContent.addEventListener("click", changeToX);
-  }
+for (var i = 0; i < allCells.length; i++){
+  allCells[i].addEventListener("click", changeValue);
 }
 
-//Change to O
-var turnO = function() {
-  function changeToO(e) {
-    e.target.innerHTML = "O";
-  }
-//Add click event listener to all cells
-  var allCells = document.getElementsByClassName("cell");
-  for (var i = 0; i < allCells.length; i++){
-    var cellContent = document.getElementsByClassName("cell")[i];
-    cellContent.addEventListener("click", changeToO);
+function changeValue(event){
+  var activeSquare = document.getElementById(event.target.id);
+  if (activeSquare.className == "cell") {
+    if(counter % 2 === 0){
+      activeSquare.innerHTML = "<p>X</p>";
+    } else {
+      activeSquare.innerHTML = "<p>O</p>";
+    }
+
+    counter ++;
+  } else {
+    return;
   }
 }
-var counter = 0;
-while(counter < 9){
-  if (counter % 2 === 0){
-    counter ++;
-    turnX();
-  }
-  else {
-    counter ++;
-    turnO();
-  }
-}
-// function turnSwitch(){
-//     if (buttonText == "Turn X") {
-//       document.getElementsByClassName("turnSwitchBtn")[0].innerHTML = "Turn O";
-//       console.log(buttonText);
-//       console.log(document.getElementsByClassName("turnSwitchBtn")[0].innerHTML);
-//       turnO();
-//     }
-//     else if (buttonText == "Turn O"){
-//       document.getElementsByClassName("turnSwitchBtn")[0].innerHTML = "Turn X";
-//       console.log(buttonText);
-//       console.log(document.getElementsByClassName("turnSwitchBtn")[0].innerHTML);
-//       turnX();
-//     }
-// }
-// console.log(buttonText);
-// for(var turn = 0; turn <9; turn++){
-//   button.addEventListener("click", turnSwitch);
-// }
