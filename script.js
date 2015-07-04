b72e5ec5d02727345059540debf698c9b1d39ca7
Edward Park <parkes.edward@gmail.com>
2015-07-03 21:28:00 -0400

    changed span to accomodate getElementsByTagName class difff + FOR loop to addEventListener to all buttons

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 //v.0.1
-
+var counter = 1;
+var block = document.body.getElementsByTagName("span");
 //as a user I want to click on the box and have it alernate between X and o
 //as a user I want to click all the boxes and have it say draw
 //as a user I want to reset the gameboard
+function playerMove(event){
+  if (event.target.innerHTML.length === 0){
+  
+}
+
+
+for (i=block.length -1 ; i >= 0 ; i--){
+  block[i].addEventListener("click",playerMove);
+}//remember because live you have to start from the bottom node
