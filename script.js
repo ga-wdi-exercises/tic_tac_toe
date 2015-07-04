92c214fb1194886938854df7569d3f5eb6c80203
Edward Park <parkes.edward@gmail.com>
2015-07-03 21:59:07 -0400

    added button class and functionality for clearboard function

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 //v.0.1
 var counter = 1;
 var block = document.body.getElementsByTagName("span");
+var resetBoard = document.getElementById("clear");
+//primary diff is understanding what types are returned
+//and approp. methods and avail attr you can use!
+
+
+function clearBoard(event){
+  
+
+}
 //as a user I want to click on the box and have it alernate between X and o
 //as a user I want to click all the boxes and have it say draw
 //as a user I want to reset the gameboard
@@ -18,7 +27,8 @@ function playerMove(event){
   }
 }
 
-
 for (i=block.length -1 ; i >= 0 ; i--){
   block[i].addEventListener("click",playerMove);
 }//remember because live you have to start from the bottom node
+
+resetBoard.addEventListener("click",clearBoard);
