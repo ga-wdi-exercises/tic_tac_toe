cd645c565f8610a5f197179004e1fd9e546f6fd8
Erica Irving <contact.eirving@gmail.com>
2015-07-05 19:18:36 -0400

    It works. I am so excited. Goal for next week is to get better about useful and informative commis.

--- script.js
+++ script.js
@@ -1,72 +1,147 @@
 var markers = document.body.querySelectorAll(".marker")
-var freeSpaces = document.body.querySelectorAll(".free")
-var markedSpaces = document.body.querySelectorAll(".marked")
+var playableSpaces = document.body.querySelectorAll(".playable_square")
+var topRow = document.getElementById("top_row").children
+var middleRow = document.getElementById("middle_row").children
+var bottomRow = document.getElementById("bottom_row").children
+var counter = []
+
+function playTopLeft() {
+  counter.push(topRow[0]);
+  if(counter.length % 2 === 0){
+    topRow[0].className = "markedO"
+    markers[0].innerHTML = "O"
+  }
+  else if (counter.length % 2 !== 0){
+    topRow[0].className = "markedX"
+    markers[0].innerHTML = "X"
+  }
+  return counter.length
+}
+
+function playTopMiddle() {
+  counter.push(topRow[1]);
+  if(counter.length % 2 === 0){
+    topRow[1].className = "markedO"
+    markers[1].innerHTML = "O"
+  }
+  else if (counter.length % 2 !== 0){
+    topRow[1].className = "markedX"
+    markers[1].innerHTML = "X"
+  }
+  return counter.length
+  }
+
+function playTopRight() {
+  counter.push(topRow[2]);
+  if(counter.length % 2 === 0){
+    topRow[2].className = "markedO"
+    markers[2].innerHTML = "O"
+  }
+  else if (counter.length % 2 !== 0){
+    topRow[2].className = "markedX"
+    markers[2].innerHTML = "X"
+  }
+  return counter.length
+}
+
+function playMiddleLeft() {
+  counter.push(middleRow[0]);
+  if(counter.length % 2 === 0){
+    middleRow[0].className = "markedO"
+    markers[3].innerHTML = "O"
+  }
+  else if (counter.length % 2 !== 0){
+    middleRow[0].className = "markedX"
+    markers[3].innerHTML = "X"
+  }
+  return counter.length
+}
+
+function playCenterSquare() {
+  counter.push(middleRow[1]);
+  if(counter.length % 2 === 0){
+    middleRow[1].className = "markedO";
+    markers[4].innerHTML = "O"
+  }
+  else if (counter.length % 2 !== 0){
+    middleRow[1].className = "markedX";
+    markers[4].innerHTML = "X"
+  }
+  return counter.lengthmiddl
+  }
+
+function playMiddleRight() {
+  counter.push(middleRow[2]);
+  if(counter.length % 2 === 0){
+    middleRow[2].className = "markedO"
+    markers[5].innerHTML = "O"
+  }
+  else if (counter.length % 2 !== 0){
+    middleRow[2].className = "markedX"
+    markers[5].innerHTML = "X"
+  }
+  return counter.length
+}
+
+function playBottomLeft() {
+  counter.push(bottomRow[0]);
+  if(counter.length % 2 === 0){
+    bottomRow[0].className = "markedO"
+    markers[6].innerHTML = "O"
+  }
+  else if (counter.length % 2 !== 0){
+    bottomRow[0].className = "markedX"
+    markers[6].innerHTML = "X"
+  }
+  return counter.length
+}
+
+function playBottomMiddle() {
+  counter.push(bottomRow[1]);
+  if(counter.length % 2 === 0){
+    bottomRow[1].className = "markedO";
+    markers[7].innerHTML = "O"
+  }
+  else if (counter.length % 2 !== 0){
+    bottomRow[1].className = "markedX";
+    markers[7].innerHTML = "X"
+  }
+  return counter.length
+  }
+
+function playBottomRight() {
+  counter.push(bottomRow[2]);
+  if(counter.length % 2 === 0){
+    bottomRow[2].className = "markedO"
+    markers[8].innerHTML = "O"
+  }
+  else if (counter.length % 2 !== 0){
+    bottomRow[2].className = "markedX"
+    markers[8].innerHTML = "X"
+  }
+  return counter.length
+}
 
 function reset(){
-  for (i = 0; i < markedSpaces.length; i ++) {
-    markedSpaces[i].className = "free"
+  for (i = 0; i < playableSpaces.length; i ++) {
+    playableSpaces[i].className = "playable_square"
   }
   for (i = 0; i < (markers.length - 1); i ++) {
     markers[i].innerHTML = "&nbsp;"
   }
+  counter = []
+  return true
 }
 
-document.getElementById("reset_button").addEventListener("click",reset)
+document.getElementById("reset_button").addEventListener("click",reset);
+topRow[0].addEventListener("click", playTopLeft);
+topRow[1].addEventListener("click", playTopMiddle);
+topRow[2].addEventListener("click", playTopRight);
+middleRow[0].addEventListener("click", playMiddleLeft);
+middleRow[1].addEventListener("click", playCenterSquare);
+middleRow[2].addEventListener("click", playMiddleRight);
+bottomRow[0].addEventListener("click", playBottomLeft);
+bottomRow[1].addEventListener("click", playBottomMiddle);
+bottomRow[2].addEventListener("click", playBottomRight);
 
-//function playAnX() {
-//  freeSpace = markedSpace
-//  if text = "X"
-  //When freeSpace is clicked, set class to marked_space and innerHTML to "X"
-//}
-
-//function playAnO() {
-//  freeSpace = markedSpace
-//  text = "O"
-  //When freeSpace is clicked, set class to marked_space and innerHTML to "O"
-//}
-
-//function playTicTacToe() {
-//  for (i=1; i < 10; i++) {
-//    if(i % 2 === 0) {
-//      playAnO();
-//    }
-//    else {
-//      playAnX();
-//    }
-    //i++
-//  }
-//}
-
-
-document.body.querySelector(".free_space").addEventListener("click", function)
-
-
-//var squareOne = document.getElementById("square_one")
-//var squareTwo = document.getElementById("square_two")
-//var squareThree = document.getElementById("square_three")
-//var squareFour = document.getElementById("square_four")
-//var squareFive = document.getElementById("square_five")
-//var squareSix = document.getElementById("square_six")
-//var squareSeven = document.getElementById("square_seven")
-//var squareEight = document.getElementById("square_eight")
-//var squareNine = document.getElementById("square_nine")
-
-//markerOne.innerHTML = "&nbsp;"
-//markerTwo.innerHTML = "&nbsp;"
-//markerThree.innerHTML = "&nbsp;"
-//markerFour.innerHTML = "&nbsp;"
-//markerFive.innerHTML = "&nbsp;"
-//markerSix.innerHTML = "&nbsp;"
-//markerSeven.innerHTML = "&nbsp;"
-//markerEight.innerHTML = "&nbsp;"
-//markerNine.innerHTML = "&nbsp;"
-
-//var markerOne = document.getElementById("marker_one")
-//var markerTwo = document.getElementById("marker_two")
-//var markerThree = document.getElementById("marker_three")
-//var markerFour = document.getElementById("marker_four")
-//var markerFive = document.getElementById("marker_five")
-//var markerSix = document.getElementById("marker_six")
-//var markerSeven = document.getElementById("marker_seven")
-//var markerEight = document.getElementById("marker_eight")
-//var markerNine = document.getElementById("marker_nine")
+//At a later date: make played squares unclickable; limit to 9 moves; make it baseball themed,
