f88139006fbd46f962ca186359def46af376cb65
Omari Mayers-Walker <omari@fastmail.fm>
2015-07-05 19:03:13 -0400

    Completed project.

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-var playerSqSelect;
+var playerSqSelect = "";
+var playerChoice = "";
+var takenSquares = [];
+var clickedIdSelected = "";
+var lastMove = "";
 
-function playerSelected(clickedId) {
-    console.log(clickedId);
-    playerSqSelect = clickedId;
-    document.getElementById(playerSqSelect).innerHTML = "O";
-    document.getElementById(playerSqSelect).style.color = "cyan";
-    return playerSqSelect;
+function squareStatus() {
+
+    if (playerSqSelect !== "") {
+        return false;
+    } else return true;
+
 }
 
+function squareAvailibility() {
+    if(takenSquares.indexOf(clickedIdSelected) > -1) {
+            console.log(takenSquares);
+            alert("That square is already taken!");
+        }
+        else {
+            XorO();
+        }
+}
 
+function XorO(playerChoice) {
+    if (lastMove == "" || lastMove == "X"){
+        document.getElementById(clickedIdSelected).innerHTML = "O";
+        document.getElementById(clickedIdSelected).style.color = "cyan";
+        document.getElementById("button").value = "Player X's Move";
+        lastMove = "O";
+    } else {
+        document.getElementById(clickedIdSelected).innerHTML = "X";
+        document.getElementById(clickedIdSelected).style.color = "purple";
+        document.getElementById("button").value = "Player O's Move";
+        lastMove ="X";
+    }
+}
+
+function playerSelected(clickedId) {
+    var squareStatusResult = squareStatus();
+
+    if (squareStatus() == true){
+        clickedIdSelected = clickedId;
+        console.log(takenSquares);
+        squareAvailibility();
+        takenSquares.push(clickedIdSelected);
+    }
+
+}
 
