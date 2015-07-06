7c18ef8e7a0106c15daf16e2f551940e1c44d961
Matt Ficke <matthew.ficke@gmail.com>
2015-07-05 23:15:54 -0400

    mark box with alternating x or o

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 var grid = document.querySelector("#grid");
-
+var whichPlayer = "X"
 grid.addEventListener("click", markSquare);
 
 function markSquare(square) {
   if (square.target != square.currentTarget) {
     var clicked = square.target.id;
-    document.getElementById(clicked).innerHTML = "X";
   }
+  playerMove(clicked);
+}
+
+function playerMove(clicked) {
+    if (whichPlayer === "O") {
+      //player 2 move
+      document.getElementById(clicked).innerHTML = "O";
+      whichPlayer = "X";
+    }
+    else {
+      //player 1 move
+      document.getElementById(clicked).innerHTML = "X";
+      whichPlayer = "O";
+    }
 }
