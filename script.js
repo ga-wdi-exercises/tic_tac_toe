5e7818a5bb006aed2c271ae4d5b0723a55551563
Matt Ficke <matthew.ficke@gmail.com>
2015-07-05 23:05:09 -0400

    fixing error if you click in between grid lines

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ var grid = document.querySelector("#grid");
 
 grid.addEventListener("click", markSquare);
 
-function markSquare(square) {  
+function markSquare(square) {
+  if (square.target != square.currentTarget) {
     var clicked = square.target.id;
     document.getElementById(clicked).innerHTML = "X";
-
+  }
 }
