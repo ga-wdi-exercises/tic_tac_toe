9a499c224eb57f8c918f10b9ebde9cac5b82b5db
Malissa Cruz Romero <romero.malissa@gmail.com>
2015-07-05 20:22:54 -0400

    Final JS

---
+++ script.js
@@ -0,0 +1,12 @@
+// var table = document.getElementById("table"),
+// var row = table.getElementsByTagName("tr")[0],
+// var data = row.getElementsByTagName("td")[0];
+//
+// //
+// // table.onclick = function () { alert('did stuff #1'); };
+// // row.onclick = function () { alert('did stuff #2'); };
+// // data.onclick = function () { alert('did stuff #3'); };
+//
+// for (var i = 0; i < tr.length; i++){
+//     table = tr[i].getElementsByTagName("tr");
+//   }
