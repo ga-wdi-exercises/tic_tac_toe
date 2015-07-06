e4b651c1f7df72af632e18fd27cfcc68ebca6c70
Kevin Cole Jr <KevinColeLr@gmail.com>
2015-07-06 08:37:30 -0400

    Initial commit

---
+++ myScript.js
@@ -0,0 +1,26 @@
+/*var pleaseWork =document.getElementById('one');
+
+pleaseWork.onclick = function(){
+	alert("you clicked box one");
+}*/
+
+//Box seven
+var changeColor = document.getElementById('seven');
+
+changeColor.onmouseover = function() {
+	alert("This is hard");
+}
+
+//Box One
+var changeColor = document.getElementById('one');
+
+changeColor.onclick = function() {
+	alert("Late Nights early mornings");
+}
+
+//Box Two
+var changeColor = document.getElementById('two');
+
+changeColor.onclick = function() {
+	alert("I should take a break and come back with fresh eyes");
+}
