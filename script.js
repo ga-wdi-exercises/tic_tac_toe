1dfad0aa79380e3318a21e79f6baf1bb46d5bce2
Arash Shahry <arash.shahry@gmail.com>
2015-07-05 23:23:46 -0400

    tic-tac-toe

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var cells = document.getElementsByClassName("zero");
+var cells = document.getElementsByClassName("one");
+var cells = document.getElementsByClassName("two");
+var cells = document.getElementsByClassName("three");
+var cells = document.getElementsByClassName("zero");
+var cells = document.getElementsByClassName("zero");
+var cells = document.getElementsByClassName("zero");
+var cells = document.getElementsByClassName("zero");
+var cells = document.getElementsByClassName("zero");
