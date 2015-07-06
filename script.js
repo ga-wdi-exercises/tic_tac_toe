6e4566bed43140a5261d43472061d24d1b25cff9
Justin Price <Justin@Justins-MacBook-Pro.local>
2015-07-06 08:55:20 -0400

    added to nextTurn function to display red or blue based on turn

--- script.js
+++ script.js
@@ -20,12 +20,26 @@ function nextTurn(square) {
 function nextMove() {
   if (document.turn == "X") {
     document.turn = "O";
+    document.getElementById("squareOne").style.color = "blue";
+    document.getElementById("squareTwo").style.color = "blue";
+    document.getElementById("squareThree").style.color = "blue";
+    document.getElementById("squareFour").style.color = "blue";
+    document.getElementById("squareFive").style.color = "blue";
+    document.getElementById("squareSix").style.color = "blue";
+    document.getElementById("squareSeven").style.color = "blue";
+    document.getElementById("squareEight").style.color = "blue";
+    document.getElementById("squareEight").style.color = "blue";
   } else {
     document.turn = "X";
+    document.getElementById("squareOne").style.color = "red";
+    document.getElementById("squareTwo").style.color = "red";
+    document.getElementById("squareThree").style.color = "red";
+    document.getElementById("squareFour").style.color = "red";
+    document.getElementById("squareFive").style.color = "red";
+    document.getElementById("squareSix").style.color = "red";
+    document.getElementById("squareSeven").style.color = "red";
+    document.getElementById("squareEight").style.color = "red";
+    document.getElementById("squareEight").style.color = "red";
   }
     xOrO(document.turn + " goes now!");
-}
-
-function changeColor(square) {
-  square.style.backgroundColor = "blue";
 }
