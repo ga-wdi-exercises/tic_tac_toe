0a632884ebf1161b755a2c96ebd96c92a9b43646
Jeff Jones <jeff.jones1@gmail.com>
2015-07-05 21:54:59 -0400

    Final stopping point for Tic Tac Toe

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
 // Testing on one cell clicking to change to
 // X and adding another class to cell
 
-var test = document.getElementById('c3')
+var example = document.getElementById('c3')
 
-function tableReturn (){
+function exampleBox () {
+    if (example.innerHTML =="") {
+    example.innerHTML = 'X';
+    example.className += ' blue'
+    example.style.color = "blue";
+  }
 
-    test.innerHTML = 'X'
-    test.className += ' blue'
+    else if (example.innerHTML == 'X') {
+      example.innerHTML = 'O';
+      example.className += ' red'
+      example.style.color = "red";
+    }
+}
+example.addEventListener("click", exampleBox, false);
 
+var cellSelect = document.getElementsByClassName('cell');
+
+function clickChange () {
+
+    for (i = 0; i < cellSelect.length; i++) {
+       if (cellSelect[i].innerHTML == " "){
+         cellSelect [i].innerHTML == " X"
+       }
+    }
 }
 
-test.addEventListener("click", tableReturn, false);
+addEventListener("click", clickChange, false);
+
+var allCells = document.getElementsByClassName('cell');
+
+
 
 var clear = document.getElementsByClassName ('gameboard')
 // Attempting to use a for loop to iterate over elements of the 'gameboard'
 // array in order to reset, by passing i in for each of the classes c1-c9.
 // But it appears that not all of the table elements count as individual pieces of the array
 
-var finish = document.querySelector('button')
+var finish = document.getElementsByClassName('reset')
 
 function resetBoard () {
-  for (i = 0; i < clear; i ++){
-    document.querySelector().innerHTML = '';
+  for (i = 0; i < allCells.length; i ++){
+    allCells[i].innerHTML = '';
   }
 }
-
-finish.addEventListener("click", resetBoard, false);
