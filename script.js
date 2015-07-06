85027edcd46a025204640b27467ab2169e30eafc
Lisa <lscottcook@hotmail.com>
2015-07-05 23:02:06 -0400

    homework

---
+++ script.js
@@ -0,0 +1,35 @@
+var playerMove= "x"
+var nboard=9
+function onclick(playerMove);
+
+
+
+
+/*Player move
+
+player clicks on square
+square marks a n x
+square changes to green
+switch quare is clicked
+squares marks an o
+square changes to blue
+repeate
+no 2 squares clicked
+reset button
+*/
+
+
+function onClick(){
+  if (getElementByID.A1.value= "x" || "o"){ //check for move
+    alert("Please make a different move");
+  }
+
+else if (getElementByID.A1.value= " "){// place x
+  form.A1.value = "x";
+  form.mover.value = "O";
+}
+else {
+  for(var i = 0; i < nboard; i++){
+    document.getElementID ("A1") .addEventListener("click", "x");
+  getElementbyID.A1[i].mover.value = "x"
+ }
