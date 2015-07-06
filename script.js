409b2ebe329898ecfd18fa11b18e1b0417ccda0b
lanak1301 <musiconstellation@gmail.com>
2015-07-06 08:30:42 -0400

    more of not finished stuff

--- script.js
+++ script.js
@@ -21,7 +21,12 @@ function makeMove() {
 }
 
 function checkWinner() {
+	winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 	
+	document.getElementById(smallArray[0]).innerHTML && 
+	document.getElementById(smallArray[0]).innerHTML === 
+	document.getElementById(smallArray[1]).innerHTML === 
+	document.getElementById(smallArray[2]).innerHTML
 }
 
 function clearBoard() {
