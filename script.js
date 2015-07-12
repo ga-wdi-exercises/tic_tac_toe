0a38da26ab797a6c9944e6c4d7b6a78eec52bb9f
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-11 23:30:29 -0400

    board is functional

--- script.js
+++ script.js
@@ -5,23 +5,38 @@
 // 4. a move can't be undone
 // 5. add reset button to clear board
 
-// Outline:
-// 1. listen for a click event on a box
-// 2. when click on a box is executed, toggle between marking x and o
-
 var numberOfClicks = 0;
-var getMoves = document.getElementsByClassName('square');
+var getMoves = document.getElementsByClassName("square");
+
+var b1 = document.getElementById("box1").addEventListener("click", changeButtonDesign);
+var b2 = document.getElementById("box2").addEventListener("click", changeButtonDesign);
+var b3 = document.getElementById("box3").addEventListener("click", changeButtonDesign);
+var b4 = document.getElementById("box4").addEventListener("click", changeButtonDesign);
+var b5 = document.getElementById("box5").addEventListener("click", changeButtonDesign);
+var b6 = document.getElementById("box6").addEventListener("click", changeButtonDesign);
+var b7 = document.getElementById("box7").addEventListener("click", changeButtonDesign);
+var b8 = document.getElementById("box8").addEventListener("click", changeButtonDesign);
+var b9 = document.getElementById("box9").addEventListener("click", changeButtonDesign);
 
 for (var i = 0; i < getMoves.length; i++) {
   (function(i){
-  getMoves[i].addEventListener("click", toggleButtonDesign);
+  getMoves[i].addEventListener("click", trackClicks);
   })(i);
 }
-  function toggleButtonDesign() {
-    numberOfClicks++;
+
+function trackClicks() {
+  numberOfClicks++;
+  document.getElementsByClassName("square").innerHTML = numberOfClicks;
+}
+
+function changeButtonDesign() {
     if (numberOfClicks % 2 === 0) {
-    document.getElementById("box1").classList.toggle("green");
+      this.style.backgroundColor = "mediumaquamarine";
+      this.innerHTML = "x";
   } else if (numberOfClicks % 2 !==0) {
-    document.getElementById("box1").classList.toggle("blue");
+      this.style.backgroundColor = "skyblue";
+      this.innerHTML = "o";
   }
 }
+
+// $.cookie('stored_clicks');
