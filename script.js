7b399b5bf367a69e728a3f1faa738a13a25d1680
Rebecca G. Estes <estes.rebecca@gmail.com>
2015-10-23 11:36:29 -0400

    game really works

--- script.js
+++ script.js
@@ -7,19 +7,50 @@ $(".gameboard").css("width", "350px")
 
 $(".column").css("float", "left");
 
-var totalClick = 0;
+var squaresChanged = 0;
+var lastSquare;
 
 $(".square").on("click", function() {
-	totalClick++;
 	if ($(this).html() != "<h1>X</h1>" && $(this).html() != "<h1>O</h1>")  {
-		if (totalClick % 2 === 1) {
-			$(this).html("<h1>X</h1>");
-		} else {
+		if (squaresChanged % 2 === 1) {
 			$(this).html("<h1>O</h1>");
+			squaresChanged ++;
+		} else {
+			$(this).html("<h1>X</h1>");
+			squaresChanged ++;
 		}
 	}
-})
+	lastSquare = ($(this).text());
+	if (squaresChanged >= 5) {
+		whoWon();
+	}
+	})
+
 
 $("button").on("click", function() {
-	$(".square").css("background", "white");
+	$(".square").html("");
 })
+
+function whoWon() {
+	var s1 = $(".square").eq(0).html();
+	var s2 = $(".square").eq(1).html();
+	var s3 = $(".square").eq(2).html();
+	var s4 = $(".square").eq(3).html();
+	var s5 = $(".square").eq(4).html();
+	var s6 = $(".square").eq(5).html();
+	var s7 = $(".square").eq(6).html();
+	var s8 = $(".square").eq(7).html();
+	var s9 = $(".square").eq(8).html();
+	if (
+		(s1 === s2 && s2 === s3 && s1 != "") ||
+		(s4 === s5 && s5 === s6 && s4 != "") ||
+		(s7 === s8 && s8 === s9 && s7 != "") ||
+		(s1 === s4 && s4 === s7 && s1 != "") ||
+		(s2 === s5 && s5 === s8 && s2 != "") ||
+		(s3 === s6 && s6 === s9 && s3 != "") ||
+		(s1 === s5 && s5 === s9 && s1 != "") ||
+		(s3 === s5 && s5 === s7 && s3 != "") )
+		{
+		 console.log(lastSquare + " won!");
+		}
+	}
