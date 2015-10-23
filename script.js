23311a12b02d325f59d9621165d0a61e6cfad4a6
Starr Z. Chen <chenstarr@gmail.com>
2015-10-23 09:48:11 -0400

    Grid of squares

---
+++ script.js
@@ -0,0 +1,7 @@
+$(".square").css("height", "100px");
+$(".square").css("width", "100px");
+$(".square").css("border", "solid black 2px");
+$(".gameboard").css("display", "inline-block");
+$(".gameboard").css("width", "350px")
+
+$(".column").css("float", "left");
