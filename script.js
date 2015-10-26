b98530b9addc69db7504f8216a34568c9ca8008e
Jorge <jlpimentel@gmail.com>
2015-10-26 10:53:26 -0400

    second commit, added css

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   $(".square").css("background-color","white");
 });
-
 var ticTacToe = {
   oTurn: 0,
   clickListeners: function(){
@@ -9,14 +8,17 @@ var ticTacToe = {
   },
   x_and_o: function(){
     if(ticTacToe.oTurn === 0){
-      $(this).html("<h1>X</h1>")
-      ticTacToe.oddeven = 1;
+      $(this).html("<h1>X</h1>");
+      ticTacToe.oTurn = 1;
     } else if (ticTacToe.oTurn === 1){
-      $(this).html("<h1>O</h1>")
-      ticTacToe.oddeven = 0;
+      $(this).html("<h1>O</h1>");
+      ticTacToe.oTurn = 0;
     }
-
   }
-}
-
+};
 ticTacToe.clickListeners();
+
+var reset = document.getElementById("reset");
+reset.addEventListener("click", function(){
+  alert("this one works too!");
+});
