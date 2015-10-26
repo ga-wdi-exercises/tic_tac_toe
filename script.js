1664ebe7d12b4a275863b36f3db481f1efd0e63d
Sigmund Nathan Tang <shindigira@gmail.com>
2015-10-26 14:15:21 -0400

    reset works in JS

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 $(document).ready(function(){
   $(".square").css("background-color","white");
 });
+
 var ticTacToe = {
   oTurn: 0,
   clickListeners: function(){
-    $(".square").on("click", this.x_and_o);
+    $(".square").on("click", this.x_and_o.bind(this));
+    $("#reset").on("click", this.resetGame);
   },
-  x_and_o: function(){
+  x_and_o: function(e){
     if(ticTacToe.oTurn === 0){
-      $(this).html("<h1>X</h1>");
-      ticTacToe.oTurn = 1;
+      $(e.target).html("<h1>X</h1>");
+      this.oTurn = 1;
     } else if (ticTacToe.oTurn === 1){
-      $(this).html("<h1>O</h1>");
-      ticTacToe.oTurn = 0;
+      $(e.target).html("<h1>O</h1>");
+      this.oTurn = 0;
     }
+  },
+  resetGame: function(){
+    $(".square").html("");
   }
 };
 ticTacToe.clickListeners();
 
-var reset = document.getElementById("reset");
-reset.addEventListener("click", function(){
-  alert("this one works too!");
-});
+// var reset = document.getElementById("reset");
+// reset.addEventListener("click", function(){
+//   alert("this one works too!");
+// });
