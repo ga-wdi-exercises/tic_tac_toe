ce9423c45f612a870b9d82eb0998d9afb6812c64
Jorge <jlpimentel@gmail.com>
2015-10-26 11:27:31 -0400

    first commit, style is done... still needs functionality

---
+++ script.js
@@ -0,0 +1,52 @@
+//var boxes = document.getElementsByClassName("boxes");//
+var reset = document.getElementById("reset");
+reset.addEventListener("click", function(){
+  alert("this one works too!");
+});
+
+  var ceroBox = document.getElementById("box0");
+    ceroBox.addEventListener("click", function(){
+        ceroBox.innerHTML= "X";
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //
+    // function addHandler(){
+    //   }
+    //       document.getElementById('myButton').onclick = function(){
+    //       alert("it works!");
+    //         };
+
+
+// $("#box0").on("click", function() { /// this is the Jquery method//
+//   $("#box0").html("X")
+// })
