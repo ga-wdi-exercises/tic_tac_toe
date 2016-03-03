1b734dbfe7d4704c1cdfa65efb2a8b72aa5ae315
Melissa Miller <lis.d.miller@gmail.com>
2016-03-03 10:03:36 -0500

    Creates the board, adds script files

---
+++ script.js
@@ -0,0 +1,17 @@
+// create a 3x3 grid
+  // get board
+  // insert divs into board
+function TicTacToe() {
+  this.board = $('.board');
+}
+
+
+
+
+
+// make grid visible
+// on click, add a character to the grid, x or o
+  // click also changes color of cell
+  // click freezes cell so it can't be clicked again
+// after each click, the character switches, x -> o, o -> x
+// reset button to clear board and restart game
