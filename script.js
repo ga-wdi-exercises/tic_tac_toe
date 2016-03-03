9838c89517a4d54472a33141059bb1ace868d454
YOUR FULL NAME <farishta.haider@gmail.com>
2016-03-03 15:43:29 -0500

    another test

--- script.js
+++ script.js
@@ -1,43 +1,56 @@
 $(document).ready(function(){
 
-var turn = $(".turn");
-var table = $(".table");
-var reset = $("#resetButton");
-var player = 1["#x", "#o"];
-// var playerName1 = $("");
-// var playerName2 = $("");
-var player1 = $("#x");
-var player2 = $("#o");
-// var player(){
-//   this.player: [0,1],
-//   this.character: ["x","o"],
-//   this.playername: [""]
-// }
+var turn = $('.turn');
+var table = $('.table');
+var reset = $('.reset');
+var player = 1;
+var player1 = $('.X');
+var player2 = $('.O');
 
-function checkPlayed(td) {
-  if(td.hasClass("x") || td.hasClass("o")) {
+$('.grid').click(function() {
+  grid = $(this);
+  var alreadyPlayed = checkPlayed(grid);
+  if(!alreadyPlayed)
+  {
+    var pattern = pickCharacter(player);
+    changePlayed(grid, pattern);
+    if(checkIfWon(table, pattern))
+    {
+      messages.html('Player '+player+' has won.');
+      turn.html('');
+    }
+    else {
+      player = setNextPlayer(player);
+      displayNextPlayer(turn, player);
+    }
+  }
+  else {
+    messages.html('This box is already checked.');
+  }
+  $('.reset').click(function() {
+    player = 1;
+    // messages.html('');
+    reset(table);
+    displayNextPlayer(turn, player);
+  });
+
+function checkPlayed(grid) {
+  if(grid.hasClass('X') || grid.hasClass('O')) {
     return 1;
   } else {
     return 0;
   }
 }
 
-function changePlayed(td, pattern) {
-  return td.addClass(pattern);
+function changePlayed(grid, pattern) {
+  return grid.addClass(pattern);
 }
-
-$('.reset').click(function() {
-  player = 1;
-  messages.html('');
-  reset(table);
-  displayNextPlayer(turn, player);
-});
 
 function pickCharacter(player) {
   if(player == 1) {
-    return "#x";
+    return 'X';
   } else {
-    return "#o";
+    return 'O';
   }
 }
 
@@ -53,75 +66,50 @@ function displayNextPlayer(turn, player) {
   turn.html('Player turn : '+player);
 }
 
-function prompts() {
-
-}
-
 function setTurn(){
   var p = Math.floor((Math.random() * 2) + 1);
   hasWinner=0;
   if(p==1){
     turn = playerName1;
-    boardMsg(playerName1+"'s turn now!");
+    //boardMsg(playerName1+''s turn now!'');
   }
   else{
     turn = playerName2;
-    boardMsg(playerName2+"'s turn now!");
+  //  boardMsg(playerName2+''s turn now!'');
   }
 }
 function checkIfWon(table, pattern) {
   var won = 0;
-  if(table.find("#col-1-row-1").hasClass(pattern) && table.find("#col-2-row-1").hasClass(pattern) && table.find("#col-3-row-1").hasClass(pattern)) {
+  if(table.find('#col-1-row-1').hasClass(pattern) && table.find('#col-2-row-1').hasClass(pattern) && table.find('#col-3-row-1').hasClass(pattern)) {
     won = 1;
-  } else if (table.find("#col-1-row-1").hasClass(pattern) && table.find("#col-1-row-2").hasClass(pattern) && table.find("#col-1-row-3").hasClass(pattern)) {
+  } else if (table.find('#col-1-row-1').hasClass(pattern) && table.find('#col-1-row-2').hasClass(pattern) && table.find('#col-1-row-3').hasClass(pattern)) {
     won = 1;
-  } else if (table.find("#col-1-row-1").hasClass(pattern) && table.find("#col-2-row-2").hasClass(pattern) && table.find("#col-3-row-3").hasClass(pattern)) {
+  } else if (table.find('#col-1-row-1').hasClass(pattern) && table.find('#col-2-row-2').hasClass(pattern) && table.find('#col-3-row-3').hasClass(pattern)) {
     won = 1;
-  } else if (table.find("#col-1-row-2").hasClass(pattern) && table.find("#col-2-row-2").hasClass(pattern) && table.find("#col-3-row-2").hasClass(pattern)) {
+  } else if (table.find('#col-1-row-2').hasClass(pattern) && table.find('#col-2-row-2').hasClass(pattern) && table.find('#col-3-row-2').hasClass(pattern)) {
     won = 1;
-  } else if (table.find("#col-1-row-3").hasClass(pattern) && table.find("#col-2-row-3").hasClass(pattern) && table.find("#col-3-row-3").hasClass(pattern)) {
+  } else if (table.find('#col-1-row-3').hasClass(pattern) && table.find('#col-2-row-3').hasClass(pattern) && table.find('#col-3-row-3').hasClass(pattern)) {
     won = 1;
-  } else if (table.find("#col-2-row-1").hasClass(pattern) && table.find("#col-2-row-2").hasClass(pattern) && table.find("#col-2-row-3").hasClass(pattern)) {
+  } else if (table.find('#col-2-row-1').hasClass(pattern) && table.find('#col-2-row-2').hasClass(pattern) && table.find('#col-2-row-3').hasClass(pattern)) {
     won = 1;
-  } else if (table.find("#col-3-row-1").hasClass(pattern) && table.find("#col-3-row-2").hasClass(pattern) && table.find("#col-3-row-3").hasClass(pattern)) {
+  } else if (table.find('#col-3-row-1').hasClass(pattern) && table.find('#col-3-row-2').hasClass(pattern) && table.find('#col-3-row-3').hasClass(pattern)) {
     won = 1;
-  } else if (table.find("#col-3-row-1").hasClass(pattern) && table.find("#col-2-row-2").hasClass(pattern) && table.find("#col-1-row-3").hasClass(pattern)) {
+  } else if (table.find('#col-3-row-1').hasClass(pattern) && table.find('#col-2-row-2').hasClass(pattern) && table.find('#col-1-row-3').hasClass(pattern)) {
     won = 1;
   }
   return won;
 }
 
-$("#resetButton").click(function(){
+$('#resetButton').click(function(){
   player = 1;
   reset(table);
   displayNextPlayer(turn, player)
 })
 
-$("td").click(function() {
-  td = $(this);
-  var alreadyPlayed = checkPlayed(td);
-  if(!alreadyPlayed)
-  {
-    var pattern = pickCharacter(player);
-    changePlayed(td, pattern);
-    if(checkIfWon(table, pattern))
-    {
-      messages.html('Player '+player+' has won.');
-      turn.html('');
-    }
-    else {
-      player = setNextPlayer(player);
-      displayNextPlayer(turn, player);
-    }
-  }
-  else {
-    messages.html('This box is already checked.');
-  }
-});
-
-function reset(table){
-  table("td").each(function() {
-    $(this).removeClass("player");
+$('#resetButton').click(function (){
+  table('grid').each(function() {
+    $(this).removeClass('player');
   })
-}
 })
+})
+});
