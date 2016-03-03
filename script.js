5822e896b73017eb622b705bcbe207d669709321
Melissa Miller <lis.d.miller@gmail.com>
2016-03-03 16:19:43 -0500

    Cleans up formatting and comments

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
-// create a 3x3 grid
-  // get board
-  // insert divs into board
-  // make grid visible
+function TicTacToe() {
+  this.board = $('.board');
+  this.size = 9;
+  this.player = true;  // true = X. false = O
 
-  function TicTacToe() {
-    this.board = $('.board');
-    this.size = 9;
-    this.player = true;  // true = X. false = O
-
-    var ttt = this;
-    for(var i = 0; i < this.size; i++) {
-      var square = $('<div class="square"><div class="xo"></div></div>');
-      square.appendTo(this.board);
-      square.on('click', function() {
-        ttt.playGame(this);
-      });
-    }
+  var ttt = this;
+  for(var i = 0; i < this.size; i++) {
+    var square = $('<div class="square"><div class="xo"></div></div>');
+    square.appendTo(this.board);
+    square.on('click', function() {
+      ttt.playGame(this);
+    });
   }
+}
 
 // click starts the game
-  TicTacToe.prototype.playGame = function (square) {
-    if(this.player) {
-      this.makeAMark('X', square);
-    }
-    else {
-      this.makeAMark('O', square);
-    }
-  };
-  // on click, add a character to the grid, x or o
-    // click also changes color of cell
-    // click freezes cell so it can't be clicked again
-  // after each click, the character switches, x -> o, o -> x
-  TicTacToe.prototype.makeAMark = function (player, position) {
-    var xo = $(position).children();
-    $(xo).html(player);
-    $(position).addClass(player);
-    this.player = !this.player;
-    $(position).unbind('click');
-  };
+TicTacToe.prototype.playGame = function (square) {
+  if(this.player) {
+    this.makeAMark('X', square);
+  }
+  else {
+    this.makeAMark('O', square);
+  }
+};
 
+// on click, add a character to the grid, x or o
+  // click also changes color of cell
+  // click freezes cell so it can't be clicked again
+// after each click, the character switches, x -> o, o -> x
+TicTacToe.prototype.makeAMark = function (player, position) {
+  var xo = $(position).children();
+  $(xo).html(player);
+  $(position).addClass(player);
+  this.player = !this.player;
+  $(position).unbind('click');
+};
 
+// reset button to clear board and restart game
 $('#reset').on('click', function() {
   $('.square').remove();
   $(this).html('Restart');
@@ -47,13 +43,4 @@ $('#reset').on('click', function() {
 });
 
 // some other fun stuff
-$(document).ready(function() {
-  $(".festive").lettering();
-});
-
-
-
-
-
-
-// reset button to clear board and restart game
+$(".festive").lettering();
