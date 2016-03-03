bdb57fc1bf31e9b4c97cb7690a0c9818ccd943d6
Keith Prifte <kprift@gmail.com>
2016-03-03 11:17:02 -0500

    adds X and O icon and start the game logic

---
+++ psuedo.js
@@ -0,0 +1,2 @@
+Bugs
+[] Clicking the same box twice, changes the the "X" to "O"
--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-//Create board - 3 rows of 3
-  // create funtion to append the div's to the board, in order to create the board
-    // target the element to append the divs to
-    // create function to append the divs
+// Psuedo Code
+  //Create board - 3 rows of 3
+    //[x] create funtion to append the div's in order to create the board
+      //[x] target the element to append the divs to
+  // [x] Create a click event that puts an "x" or "o" in the box
+  // Need to create logic where the first click on the board is an "x" and the second click is an "o".  Repeat.
+    //Don't need to necessarily log the picks, can read the the "x" or "o" from the board
+    // could do a oneClcik and twoClick, and .pop the value everytime.
+    // could do %2 to determine whether
 
+// --> code <--
 
-// function appendBox() {
-//   // newDiv = document.createElement('div');
-//   newDiv.className = "box";
-//   $("body").append(newDiv);
-//   newDiv.addEventListener("mouseover", function() {
-//     this.style.backgroundColor = userInput.value;
-//   })
-// }
+click = 0
 
+//builds board
+for (var i = 0; i < 9; i++){
+  appendBox()
+  $(".box").eq(i).on("click", function(){
+    click++
+    this.classList.add("active" + xo())
+  })
+}
 
 function appendBox() {
   newDiv = document.createElement('div');
@@ -20,6 +27,15 @@ function appendBox() {
   $(".control").append(newDiv)
 }
 
-for (var i = 0; i < 9; i++){
-  appendBox()
+function xo(){
+  if (click % 2 === 0) {
+    return "X"
+  } else return "O"
+}
+
+function threesCompany() {
+  // var box = $(".box")
+    if (($(".box").eq(0).css("background-image") ===    $(".box").eq(1).css("background-image")) && ($(".box").eq(0).css("background-image") === $(".box").eq(2).css("background-image"))) {
+      alert("Winner")
+    } else null
 }
