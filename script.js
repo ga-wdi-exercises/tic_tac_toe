9d67bf70da6a08d2aff1ec1105f18a41cdc361e9
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-03-03 14:16:24 -0500

    enabled winning by comparing arrays

--- script.js
+++ script.js
@@ -1,43 +1,74 @@
-var turn = "X";
-
-function changeTurn() {
-  if (turn == "X") {
-    turn = "O";
-  } else {
-    turn = "X";
-  }
-  $(".turn").html(turn + "'s turn");
-}
-
-function addClick() {
-  $(".square").on("click", function() {
-    $(this).html(turn);
-    changeTurn();
-    if (turn == "X") {
-      $(this).addClass("dark");
-    } else {
-      $(this).addClass("light");
-    }
-    $(this).off();
-  });
-}
-
-function resetButton() {
-  $(".reset").on("click", function() {
-    $(".square").html("");
-    $(".square").removeClass("light");
-    $(".square").removeClass("dark");
+  $(document).ready(function() {
     addClick();
+    changeTurn();
+    resetButton();
   });
-}
+    var Xselections = [];
+    var Oselections = [];
+    var turn = "X";
 
-$( document ).ready(function() {
-  changeTurn();
-  buildBoard();
-  addClick();
-  resetButton();
-});
+    var wins = [
+      ["1", "2", "3"],
+      ["4", "5", "6"],
+      ["7", "8", "9"],
+      ["1", "4", "7"],
+      ["2", "5", "8"],
+      ["3", "6", "9"],
+      ["1", "5", "9"],
+      ["7", "5", "3"]
+  ];
 
-function buildBoard() {
-  $(".container").html('<div class="turn">X\'s turn</div><div class="row"><div class="square"></div><div class="square"></div><div class="square"></div></div><div class="row"><div class="square"></div><div class="square"></div><div class="square"></div></div><div class="row"><div class="square"></div><div class="square"></div><div class="square"></div></div><div class="reset">reset</div>');
-}
+    function changeTurn() {
+      if (turn == "X") {
+        turn = "O";
+      } else {
+        turn = "X";
+      }
+      $(".info").html(turn + "'s turn");
+      if ($("#1").html() === $("#2").html() === $("#3").html()) {
+          console.log("winner");
+        }
+    }
+
+    function addClick() {
+      $(".square").on("click", function() {
+        $(this).html(turn);
+        changeTurn();
+        if (turn == "X") {
+          $(this).addClass("dark");
+          Oselections.push(this.id);
+          for (var i = 0; i < wins.length; i++) {
+            if (containsAll(wins[i], Oselections)) {
+              console.log("O wins");
+            }
+          }
+        } else {
+          $(this).addClass("light");
+          Xselections.push(this.id);
+          for (var j = 0; j < wins.length; j++) {
+            if (containsAll(wins[j], Xselections)) {
+              console.log("X wins");
+            }
+          }
+        }
+        $(this).off();
+      });
+    }
+
+    function resetButton() {
+      $(".reset").on("click", function() {
+        $(".square").html("");
+        $(".square").removeClass("light");
+        $(".square").removeClass("dark");
+        Oselections = [];
+        Xselections = [];
+        addClick();
+      });
+    }
+
+    function containsAll(needles, haystack){
+      for(var i = 0 , len = needles.length; i < len; i++){
+         if($.inArray(needles[i], haystack) == -1) return false;
+      }
+      return true;
+    }
