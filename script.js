571c85717e20f147631dd4f1920f5daa615015a8
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-03-03 09:43:49 -0500

    added classes on click

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+$( document ).ready(function() {
+  changeTurn();
+  buildBoard();
+  addClick();
+  resetButton();
+});
+
 var turn = "X";
 
 function changeTurn() {
@@ -7,17 +14,27 @@ function changeTurn() {
     turn = "X";
   }
 }
+function buildBoard() {
+  $(".container").html('<div class="row"><div class="square"></div><div class="square"></div><div class="square"></div></div><div class="row"><div class="square"></div><div class="square"></div><div class="square"></div></div><div class="row"><div class="square"></div><div class="square"></div><div class="square"></div></div><div class="reset">reset</div>');
+}
 
+function addClick() {
+  $(".square").on("click", function() {
+    $(this).html(turn);
 
+    if (turn == "X") {
+      $(this).addClass("X");
+    } else {
+      $(this).addClass("Y");
+    }
+    changeTurn();
+  });
+}
 
-
-$(".container").html('<div class="row"><div class="square"></div><div class="square"></div><div class="square"></div></div><div class="row"><div class="square"></div><div class="square"></div><div class="square"></div></div><div class="row"><div class="square"></div><div class="square"></div><div class="square"></div></div><div class="reset">reset</div>');
-
-$(".square").on("click", function() {
-  $(this).html(turn);
-  changeTurn();
-});
-
-$(".reset").on("click", function() {
-  $(".square").html("");
-});
+function resetButton() {
+  $(".reset").on("click", function() {
+    $(".square").html("");
+    $(".square").removeClass("X");
+    $(".square").removeClass("Y");
+  });
+}
