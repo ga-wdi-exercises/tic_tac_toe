00d0fc792545029548c05f30f0f68699c92f58d8
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-03-03 17:13:21 -0500

    declares winner if you win right away

--- script.js
+++ script.js
@@ -12,26 +12,27 @@ $(document).ready(function() {
       clickCounter++;
       if ((clickCounter % 2 === 0) && ($(this).hasClass("clicked")) === false){
         $(this).html("<img class='o' src='o.png'/>");
-        console.log(this.id);
-        oScore = parseInt($(this).id) * 2 * oScore;
-        console.log(xScore);
+        oScore = this.id * 2 * oScore;
+        console.log(oScore);
       }
       else if ((clickCounter % 2 > 0) && ($(this).hasClass("clicked")) === false){
         $(this).html("<img class='x' src='x.png'/>");
-        xScore = parseInt($(this).id) * 2 * xScore;
-        console.log(oScore);
+        xScore = this.id * 2 * xScore;
+        console.log(xScore);
       }
       $(this).addClass("clicked");
       game.score();
     },
 
     score: function(){
-      if (xScore == 840 || xScore == 48 || xScore == 200 || xScore == 432 || xScore == 360 || xScore == 1296 || xScore == 640 || xScore == 1344){
-        winner.text("X wins!");
+      if (xScore == 840 || xScore == 48 || xScore == 960 || xScore == 432 || xScore == 360 || xScore == 1296 || xScore == 640 || xScore == 1344){
+        winner.html("X wins!");
+        winner.css("backgroundColor", "red");
         console.log(xScore);
       }
-      if (oScore == 840 || oScore == 48 || oScore == 200 || oScore == 432 || oScore == 360 || oScore == 1296 || oScore == 640 || oScore == 1344){
+      if (oScore == 840 || oScore == 48 || oScore == 960 || oScore == 432 || oScore == 360 || oScore == 1296 || oScore == 640 || oScore == 1344){
         winner.html("O wins!");
+        winner.css("backgroundColor", "red");
         console.log(oScore);
       }
   },
