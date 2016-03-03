6e5aa10904b776f1c0ad4ec6bb131a542b3d5183
winishasmith <winishasmith@gmail.com>
2016-03-03 12:29:43 -0500

    function and eventlistener for user move

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-function startGame (){
+function startGame(){
   document.turn = "X";
   displayMessage(document.turn + " will start.")
 }
 
-function displayMessage (msg){
-  document.getElementById("message").innerText = msg;
+function displayMessage(msg){
+  $("message").innerText = msg;
 }
 
 document.body.onload = startGame();
+
+function userMove(){
+  this.innerText = document.turn;
+}
+
+var square = document.querySelectorAll(".box");
+for ( var i=0; i< square.length; i++){
+  square[i].addEventListener("click", userMove);
+}
