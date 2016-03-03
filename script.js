c44e9a85b299737dc604af185a8e2b48399d6d99
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-03-03 15:13:06 -0500

    player variables added.

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ $(".button").click(function(){
 // everytime a player clicks on a <td> an X or an O shows in the cell.
 console.log("I was clicked!");
   // How can tell which cell was clicked on?
-
+    var playerOne = X;
+    var playerTwo = O;
   // How can we determine whats in that cell?
 
   // if the value of the cell is not what we want?
       // change it to the opposite
-    
+
  })
 
  // when one player completes his turn as X the next players turn becomes O.
