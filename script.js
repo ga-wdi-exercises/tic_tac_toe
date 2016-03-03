38cde5653606d4a97bdfaf6c2e831d34661703d6
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-03-03 15:35:24 -0500

    took away unnecessary variables.

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ $(".button").click(function(){
 // everytime a player clicks on a <td> an X or an O shows in the cell.
 console.log("I was clicked!");
   // How can tell which cell was clicked on?
-  var playerOne = X;
-  var playerTwo = O;
   var symbol = ["X","O"];
   var id = [1, 2];
   var player = function (id, symbol){
