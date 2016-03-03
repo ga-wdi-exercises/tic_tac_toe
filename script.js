357cfc3755a2831ae4f6af0023b0bc7cc662a823
Anissa Jones <anissajones10@gmail.com>
2016-03-03 12:33:08 -0500

    on click event works for 1 box.

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
     var mainBoard = new Board($("#board"));
-    console.log(mainBoard.elements.box1.html());
-    console.log(mainBoard.elements.box1.html());
 });
 
 function Board(element) {
     //this.element == mainBoard which is my entire container div
     this.element = element;
+    this.numOfMoves = 0;
     this.elements = {
         //this is saying find .box# in the mainBoard
         box1: this.element.find(".box1"),
@@ -19,4 +18,30 @@ function Board(element) {
         box8: this.element.find(".box8"),
         box9: this.element.find(".box9")
     };
+
+    this.elements.box1.on("click", function(){
+        console.log("You clicked me!");
+    });
+
+
+
+    // for(var i=0; i<this.elements.length;i++){
+    //
+    //     console.log(i);
+    //     console.log(this.elements[i]);
+    //
+    //     (this.elements[i]).on("click", function(){
+    //         console.log("You clicked me!");
+    //     });
+    // }
+
+    //user for later for switching between x's and o's
+    function changeLetter(){
+        if(this.numOfMoves % 2 === 0){
+            //enter an X
+        }else{
+            //enter an O
+        }
+    }
+
 }
