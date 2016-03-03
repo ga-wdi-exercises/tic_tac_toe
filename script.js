17fb2793116af355b156c14f0342e7bd937c9653
Cam <camlatimer@gmail.com>
2016-03-03 16:21:17 -0500

    commit7

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 $(document).ready(function() {
-
   var ticTacToe = new Game($('table'));
   ticTacToe.initCells();
 });
-
 function Game(element) {
   game = this;
   game.element = element;
   game.status = 1;
-  game.moves = [];
+  game.moves = []; // use to find a winner
   game.initCells = function() {
     for(var i = 0; i < $('td').length; i++) {
       $('td').eq(i).click(function() {
         game.handleClick($(this));
       });
     }
-  }
+  };
   game.handleClick = function(cell) {
     if(game.status === 1) {
-      cell.html('x');
-      game.moves.push('x');
+      cell.html('<p>x</p>');
+      //game.moves.push('X');
       game.status = 0;
     } else {
-      cell.html('o');
-      game.moves.push('o');
+      cell.html('O');
+      //game.moves.push('o');
       game.status = 1;
     }
     console.log(game.moves);
