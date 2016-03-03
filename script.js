70267a052f4996a03ec0984eab0476708f1445f8
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-03-03 15:03:54 -0500

    fixed click function (for good...hopefully.)

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-$(".button").click(function(){ })
+$(".button").click(function(){
+// everytime a player clicks on a <td> an X or an O shows in the cell.
+console.log("I was clicked!");
+  // How can tell which cell was clicked on?
+
+  // How can we determine whats in that cell?
+
+  // if the value of the cell is not what we want?
+      // change it to the opposite
+    
+ })
+
+ // when one player completes his turn as X the next players turn becomes O.
