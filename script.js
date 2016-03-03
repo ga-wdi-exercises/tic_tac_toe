ccdbe8585e281b44966383da22bc3001e8f4302d
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-03-03 11:40:22 -0500

    indents cleaner

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 var ttt = {
   counter: 0,
   createBoard: function() {
-                for (var i=1; i<=9; i++){
-                  var newDiv = $('<div '+'id='+i+'></div>').addClass("clickable");
-                  $('.grid').append(newDiv);
-                }
-              },
+                  for (var i=1; i<=9; i++){
+                    var newDiv = $('<div '+'id='+i+'></div>').addClass("clickable");
+                    $('.grid').append(newDiv);
+                  }
+                },
   addReset: function(){
-            $('body').append("<button>Reset</button>");
-            ttt.reset();
-          },
+              $('body').append("<button>Reset</button>");
+              ttt.reset();
+            },
   clicks:   function() {
               for (var i = 0; i < $('.clickable').length; i++){
               $('.clickable').eq(i).on("click", function() {
                 if (ttt.counter%2===0){
                  $(this).html("x").off("click").css("backgroundColor","red");
-               ttt.counter+=1;}
-                 else {$(this).html("o").off("click").css("backgroundColor","blue");
-               ttt.counter+=1;}
-              });
+                 ttt.counter+=1;}
+               else {$(this).html("o").off("click").css("backgroundColor","blue");
+                 ttt.counter+=1;}
+               });
               }
             },
 reset: function() {$('button').on("click", function() {
         $('.clickable').remove();
         ttt.counter = 0;
         ttt.createBoard();
-        ttt.clicks();});
-        }
+        ttt.clicks();
+        });
+       }
 };
 
 ttt.createBoard();
