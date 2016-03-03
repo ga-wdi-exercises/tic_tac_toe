92360cb86471c62bb52e0f2d135e72d1ff456f09
Brendan Magee <bmagee@gmail.com>
2016-03-03 11:31:34 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// User starts with either X or O
+// User clicks on a space, and that space shows an X or an O
+  // space displays the X or O, depending on which one was currently in effect
+  // turn switches to other player
+// Play continues until one of two things happens:
+    // No spaces remain
+    // A player, either X or O, has won
+// Reset button at the bottom of the page clears all entries
+
+  ////////////////
+ // Pseudocode //
+////////////////
