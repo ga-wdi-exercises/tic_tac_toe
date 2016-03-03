f6b9b908875c236641174d485f795dd4b168da31
YOUR FULL NAME <farishta.haider@gmail.com>
2016-03-03 12:24:49 -0500

    variables added

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+var turn = $("turn");
+var player = 1["#x", "#o"];
+var player1 = $("#x");
+var player2 = $("#o");
+var table = $("table");
+var reset = $("#resetButton");
+})
