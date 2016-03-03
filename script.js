b297968ce7f61ac82fef682ecf51d93e450229c5
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-03 14:04:04 -0500

    js in html

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-$(document).ready(function(){
-
-  var player;
-  var messages = $(".messages");
-  var turn = $(".turn");
-  var table = $("table");
-  getNextPlayer(turn, player);
-
-//click a cell
-$(".cell").click(function(){
-  console.log("click");
-})
-
-}
+// $(document).ready(function(){
+//
+//   var player;
+//   var messages = $(".messages");
+//   var turn = $(".turn");
+//
+//   getNextPlayer(turn, player);
+//
+// //click a cell
+// $("td").click(function(){
+//   td = $(this);
+//   var sign = chooseSign(td);
+//     if(!sign) {
+//       var pattern = patternForPlayer(player);
+//       chooseSign(td, pattern);
+//
