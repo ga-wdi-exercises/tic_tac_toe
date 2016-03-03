e0088d6d7a116e5f51ec4fe53eaf852b24bd88ac
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-03-03 16:05:09 -0500

    added conditional for tie game

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-  $(document).ready(function() {
-    playGame();
-    // changeTurn();
-    resetButton();
-  });
-    var turn = "X";
     var Xselections = [];
     var Oselections = [];
-    var wins = [
-      ["1", "2", "3"],
-      ["4", "5", "6"],
-      ["7", "8", "9"],
-      ["1", "4", "7"],
-      ["2", "5", "8"],
-      ["3", "6", "9"],
-      ["1", "5", "9"],
-      ["7", "5", "3"]
-  ];
+    var turn = "X";
+    var clicks = 0;
+    var wins = [["1", "2", "3"],
+                ["4", "5", "6"],
+                ["7", "8", "9"],
+                ["1", "4", "7"],
+                ["2", "5", "8"],
+                ["3", "6", "9"],
+                ["1", "5", "9"],
+                ["7", "5", "3"]];
 
     function playGame() {
       $(".square").on("click", function() {
         $(this).html(turn);
         changeTurn();
-        if (turn == "X") {
+        if (turn === "X") {
           $(this).addClass("dark");
+          clicks++;
           Oselections.push(this.id);
           for (var i = 0; i < wins.length; i++) {
             if (checkForWin(wins[i], Oselections)) {
@@ -30,8 +25,12 @@
               $(".square").off();
             }
           }
+          if (clicks === 9) {
+            $(".info").html("cat's game");
+          }
         } else {
           $(this).addClass("light");
+          clicks++;
           Xselections.push(this.id);
           for (var j = 0; j < wins.length; j++) {
             if (checkForWin(wins[j], Xselections)) {
@@ -39,10 +38,14 @@
               $(".square").off();
             }
           }
+          if (clicks === 9) {
+            $(".info").html("cat's game");
+          }
         }
         $(this).off();
-      });
-    }
+      });  // end .on("click",
+      resetButton();
+    } // end playGame function
 
     function changeTurn() {
       if (turn == "X") {
@@ -62,12 +65,15 @@
 
     function resetButton() {
       $(".reset").on("click", function() {
+        turn = "X";
         $(".square").html("");
         $(".info").html(turn + "'s turn");
         $(".square").removeClass("light");
         $(".square").removeClass("dark");
         Oselections = [];
         Xselections = [];
+        clicks = 0;
         playGame();
       });
     }
+$(document).ready(playGame());
