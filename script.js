1dccabe6b99f9c0bc8d5d2c80fc6a8abd96ad604
Hilary Masland <hilarymasland@gmail.com>
2016-03-03 12:11:48 -0500

    Initial commit - board & on.click working

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+// var markerX;
+// var markerO;
+var message= $(".messages");
+var turn = $(".turn");
+var table = $(".table");
+
+$(".cell").click(function(){
+  //add class of cross or circle
+  prompt("X or O")
+  $("td").addClass('');
+});
+});
