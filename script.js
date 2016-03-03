6b4809bdd4a332515f5050fe44618c281d827fee
Christine Movius <christine.movius@gmail.com>
2016-03-03 09:46:05 -0500

    when square is clicked, console log I was clicked.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+var gameBoard = $("#gameBoard");
 
+function playTicTac() {
+  gameBoard.on("click", clickSquare);
+}
+
+playTicTac();
+
+function clickSquare(event) {
+  event.preventDefault();
+  console.log("I was clicked!");
+}
