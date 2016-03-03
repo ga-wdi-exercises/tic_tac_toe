2edac2b8ca0c52525613e24eb9d4ce9f24bc3554
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-03 14:05:12 -0500

    move js to separate file again

--- script.js
+++ script.js
@@ -14,3 +14,18 @@
 //       var pattern = patternForPlayer(player);
 //       chooseSign(td, pattern);
 //
+
+//function runGame
+$(document).ready(function(){
+  startGame();
+})
+function runGame(){
+
+giveMessage($(.turn) + " starts the game.");
+}
+
+//tell user whose turn it is, winner, etc
+//function giveMessage
+function giveMessage(){
+  $(.messages).text('')
+}
