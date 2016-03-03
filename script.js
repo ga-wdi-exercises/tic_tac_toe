44da807d4fe31d3091668a9d89e5a375724c6e18
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-03-03 11:02:58 -0500

    added click event to add x

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 var ttt = {
   createBoard: function() {
                 for (var i=1; i<=9; i++){
-                  var newDiv = $('<div></div>').addClass("clickable");
+                  var newDiv = $('<div '+'id='+i+'></div>').addClass("clickable");
                   $('.grid').append(newDiv);
                 }
               },
   addReset: function(){
             $('body').append("<button>Reset</button>");
-            }
-  };
+          },
+  clicks:   function() {
+              for (var i = 0; i < $('.clickable').length; i++){
+              $('.clickable').eq(i).on("click", function() {
+                 $(this).html("x");
+                console.log(this);
+              });
+              }
+            },
+box1click: function(){
+            $("#1").on("click", function() {
+              console.log('THIS WORKS');
+            })
+          }
+};
 
 ttt.createBoard();
 ttt.addReset();
+ttt.clicks();
+ttt.box1click();
