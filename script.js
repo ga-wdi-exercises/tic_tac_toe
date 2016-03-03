8bf27ef9cfdd5d6c756e50ef117d6330cc5fe39a
Brendan Magee <bmagee@gmail.com>
2016-03-03 15:09:39 -0500

    game works, but code smells like limburger cheese

--- script.js
+++ script.js
@@ -7,17 +7,6 @@
     // A player, either X or O, has won
 // Reset button at the bottom of the page clears all entries
 
-  ////////////////
- // Pseudocode //
-////////////////
-
-// X will always go first
-// The first player will click on a square in the table
-  // the square will display an X
-  // the turn will switch to the next player
-
-
-
   var squareOne = $("#topRow .left");
   var squareTwo = $("#topRow .middle");
   var squareThree = $("#topRow .right");
@@ -31,15 +20,39 @@
 
   var playerTwo = false;
 
+function getWinner () {
+  if (((squareOne.html() === "X") && (squareTwo.html() === "X") && (squareThree.html() === "X")) ||
+      ((squareFour.html() === "X") && (squareFive.html() === "X") && (squareSix.html() === "X"))  ||
+      ((squareSeven.html() === "X") && (squareEight.html() === "X") && (squareNine.html() === "X"))  ||
+      ((squareOne.html() === "X") && (squareFour.html() === "X") && (squareSeven.html() === "X"))  ||
+      ((squareTwo.html() === "X") && (squareFive.html() === "X") && (squareEight.html() === "X"))  ||
+      ((squareThree.html() === "X") && (squareSix.html() === "X") && (squareNine.html() === "X"))  ||
+      ((squareOne.html() === "X") && (squareFive.html() === "X") && (squareNine.html() === "X"))  ||
+      ((squareThree.html() === "X") && (squareFive.html() === "X") && (squareSeven.html() === "X"))
+  ) { alert("X has won!");
+} else if (((squareOne.html() === "O") && (squareTwo.html() === "O") && (squareThree.html() === "O")) ||
+        ((squareFour.html() === "O") && (squareFive.html() === "O") && (squareSix.html() === "O"))  ||
+        ((squareSeven.html() === "O") && (squareEight.html() === "O") && (squareNine.html() === "O"))  ||
+        ((squareOne.html() === "O") && (squareFour.html() === "O") && (squareSeven.html() === "O"))  ||
+        ((squareTwo.html() === "O") && (squareFive.html() === "O") && (squareEight.html() === "O"))  ||
+        ((squareThree.html() === "O") && (squareSix.html() === "O") && (squareNine.html() === "O"))  ||
+        ((squareOne.html() === "O") && (squareFive.html() === "O") && (squareNine.html() === "O"))  ||
+        ((squareThree.html() === "O") && (squareFive.html() === "O") && (squareSeven.html() === "O"))
+    ) { alert("O has won!");
+  }
+}
+
 squareOne.click(function(){
   if (squareOne.contents().length === 0) {
     if (playerTwo === false){
       squareOne.html("X");
       playerTwo = true;
+      getWinner();
       return playerTwo;
     } else if (playerTwo === true) {
       squareOne.html("O");
       playerTwo = false;
+      getWinner();
       return playerTwo;
     }
   }
@@ -50,10 +63,12 @@ squareTwo.click(function(){
     if (playerTwo === false){
       squareTwo.html("X");
       playerTwo = true;
+      getWinner();
       return playerTwo;
     } else if (playerTwo === true) {
       squareTwo.html("O");
       playerTwo = false;
+      getWinner();
       return playerTwo;
     }
   }
@@ -64,10 +79,12 @@ squareThree.click(function(){
     if (playerTwo === false){
       squareThree.html("X");
       playerTwo = true;
+      getWinner();
       return playerTwo;
     } else if (playerTwo === true) {
       squareThree.html("O");
       playerTwo = false;
+      getWinner();
       return playerTwo;
     }
   }
@@ -78,10 +95,12 @@ squareFour.click(function(){
     if (playerTwo === false){
       squareFour.html("X");
       playerTwo = true;
+      getWinner();
       return playerTwo;
     } else if (playerTwo === true) {
       squareFour.html("O");
       playerTwo = false;
+      getWinner();
       return playerTwo;
     }
   }
@@ -92,10 +111,12 @@ squareFive.click(function(){
     if (playerTwo === false){
       squareFive.html("X");
       playerTwo = true;
+      getWinner();
       return playerTwo;
     } else if (playerTwo === true) {
       squareFive.html("O");
       playerTwo = false;
+      getWinner();
       return playerTwo;
     }
   }
@@ -106,10 +127,12 @@ squareSix.click(function(){
     if (playerTwo === false){
       squareSix.html("X");
       playerTwo = true;
+      getWinner();
       return playerTwo;
     } else if (playerTwo === true) {
       squareSix.html("O");
       playerTwo = false;
+      getWinner();
       return playerTwo;
     }
   }
@@ -120,10 +143,12 @@ squareSeven.click(function(){
     if (playerTwo === false){
       squareSeven.html("X");
       playerTwo = true;
+      getWinner();
       return playerTwo;
     } else if (playerTwo === true) {
       squareSeven.html("O");
       playerTwo = false;
+      getWinner();
       return playerTwo;
     }
   }
@@ -134,10 +159,12 @@ squareSeven.click(function(){
     if (playerTwo === false){
       squareSeven.html("X");
       playerTwo = true;
+      getWinner();
       return playerTwo;
     } else if (playerTwo === true) {
       squareSeven.html("O");
       playerTwo = false;
+      getWinner();
       return playerTwo;
     }
   }
@@ -148,10 +175,12 @@ squareEight.click(function(){
     if (playerTwo === false){
       squareEight.html("X");
       playerTwo = true;
+      getWinner();
       return playerTwo;
     } else if (playerTwo === true) {
       squareEight.html("O");
       playerTwo = false;
+      getWinner();
       return playerTwo;
     }
   }
@@ -162,14 +191,18 @@ squareNine.click(function(){
     if (playerTwo === false){
       squareNine.html("X");
       playerTwo = true;
+      getWinner();
       return playerTwo;
     } else if (playerTwo === true) {
       squareNine.html("O");
       playerTwo = false;
+      getWinner();
       return playerTwo;
     }
   }
 });
+
+// Refactor: include reset as a prototype on the constructor
 
 resetButton.click(function(){
   squareOne.html("");
