93545bae5c7aad1b72a45d2069a8ca4368c2dd80
Anissa Jones <anissajones10@gmail.com>
2016-03-03 11:36:52 -0500

    created constructor function and now able to grab each box.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-    
+    var mainBoard = new Board($("#board"));
+    console.log(mainBoard.elements.box1.html());
+    console.log(mainBoard.elements.box1.html());
 });
+
+function Board(element) {
+    //this.element == mainBoard which is my entire container div
+    this.element = element;
+    this.elements = {
+        //this is saying find .box# in the mainBoard
+        box1: this.element.find(".box1"),
+        box2: this.element.find(".box2"),
+        box3: this.element.find(".box3"),
+        box4: this.element.find(".box4"),
+        box5: this.element.find(".box5"),
+        box6: this.element.find(".box6"),
+        box7: this.element.find(".box7"),
+        box8: this.element.find(".box8"),
+        box9: this.element.find(".box9")
+    };
+}
