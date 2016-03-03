5ddbf64920e36cf447376be5ea970769e91bef18
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-03 11:28:54 -0500

    naming variables

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
 
+  var player;
+  var messages = $(".messages");
+  var turn = $(".turn");
+  var table = $("table");
+
+//click a cell
+$("button").on("click",function(){
+
+  console.log("clicked");
+})
 }
