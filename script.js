03f095a8f49d61f0b8b46e6a5dadc0fcfecce252
Cam <camlatimer@gmail.com>
2016-03-03 15:58:39 -0500

    commit6

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-console.log('scripts up...')
-//initialize cells with event listeners and no text
-var gameCells = $('td');
-for(var i = 0; i < gameCells.length; i++) {
-  gameCells.eq(i).click(function() {
-    ticTacToe.handleClick($(this));
-  });
-  gameCells.eq(i).prop('id', 'cell' + i);
-}
-var table = $('table');
-var ticTacToe = new Game(table);
+$(document).ready(function() {
 
-// make everything run through a game element
+  var ticTacToe = new Game($('table'));
+  ticTacToe.initCells();
+});
+
 function Game(element) {
   game = this;
   game.element = element;
   game.status = 1;
   game.moves = [];
-  game.reset = $('h2');
-  game.winCheck = function() {
-    //method to loop thru array and see if consecutive moves happened
-  };
+  game.initCells = function() {
+    for(var i = 0; i < $('td').length; i++) {
+      $('td').eq(i).click(function() {
+        game.handleClick($(this));
+      });
+    }
+  }
   game.handleClick = function(cell) {
     if(game.status === 1) {
       cell.html('x');
+      game.moves.push('x');
       game.status = 0;
     } else {
       cell.html('o');
+      game.moves.push('o');
       game.status = 1;
     }
-    console.log(cell);
+    console.log(game.moves);
   };
-  game.reset.click(function() {
+  $('h2').click(function() {
     game.status = 1;
-    gameCells.html('');
+    $('td').html('');
   })
 }
