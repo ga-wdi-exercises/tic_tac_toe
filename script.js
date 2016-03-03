d431cc779f2ca53131118e8f775a44a067c659b7
Hilary Masland <hilarymasland@gmail.com>
2016-03-03 16:25:22 -0500

    Updates - still need to disable cells.

--- script.js
+++ script.js
@@ -1,51 +1,49 @@
-    $(document).ready(function(){
+      $(document).ready(function(){
 
-    var message= $(".messages");
-    var turn = $(".turn");
-    var table = $(".table");
-    var cells = $(".cell");
-    var reset = $(".reset");
-    var counter= 0;
+      var message= $(".messages");
+      var turn = $(".turn");
+      var table = $(".table");
+      var cells = $(".cell");
+      var reset = $(".reset");
+      var counter= 0;
 
 
-    function playerMoves(){
-     if (counter == 0){
-    $(this).text("X");
-    counter++;
-    } else if (counter == 1){
-    $(this).text("O");
-    counter--;
-  }
-  }
-  }
+      function playerMoves(){
+       if (counter == 0){
+      $(this).text("X").addClass("x");
+      counter++;
+      } else if (counter == 1){
+      $(this).text("O").addClass("o");
+      counter--;
+    }
+  };
 
-  // function eventListener(){
-  //     cells.click(addLetter);
-  //   }
 
-  // function addLetter(){
+    function eventListener(){
+     cells.click(playerMoves);
+
+    }
+
+    eventListener();
+  });
+  //
+  //   function addLetter(){
   //   console.log(this);
   //   $(this).text("X");
-  // }
-
-  // eventListener();
+  //   }
   //
-  // function eventListenerTwo() {
+  //  eventListener();
+  //
+  //
+  //   function eventListenerTwo() {
   //   cells.click(addLetterTwo);
-  // }
-
-  // function addLetterTwo(){
+  //    }
+  //
+  //   function addLetterTwo(){
   //   console.log(this);
   //   $(this).text("O");
   // }
-
-  eventListenerTwo();
-
-
-      });
+  //
+  // eventListenerTwo();
 
 //
-// function Cell(row, boxId) {
-//   var row = row,
-//   var boxid = boxId;
-// }
