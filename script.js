784d6cafeb440fc43e5f2eb4500ec3e31e8d9aab
Toni Langley <chibia2@gmail.com>
2016-03-03 12:42:16 -0500

    ACTUALLY added click events on all individual table cells

--- script.js
+++ script.js
@@ -1,40 +1,30 @@
 $(document).ready(function(){
 
+//array of board places  
+  var places =[
+    $("#ichi"),
+    $("#ni"),
+    $("#san"),
+    $("#jyuuichi"),
+    $("#jyuuni"),
+    $("#jyuusan"),
+    $("#nijyuuichi"),
+    $("#nijyuuni"),
+    $("#nijyuusan")
+  ];
 
-  //game places object
-  var places ={
+  $("#gameboard").hover(function(){
 
-    1: $("#ichi"),
-    2: $("#ni"),
-    3: $("#san"),
-    4: $("#jyuuichi"),
-    5: $("#jyuuni"),
-    6: $("#jyuusan"),
-    7: $("#nijyuuichi"),
-    8: $("#nijyuuni"),
-    9: $("#nijyuusan")
-  };
+    //click events on each individual td
+    for( var i = 0; i < places.length; i++ ){
+      (function(area) {
+        $(area).click(function(){ console.log(area);
+        });
+      })(places[i]);
+    }
+  });
 
-
-  // $("#gameboard").click(function(){
-
-  //ichi's text replaces jyuusan
-  // var temp =$("#ichi").text();
-  // $("#ichi").text($("#jyuusan").val());
-  // $("#jyuusan").text(temp);
-
-  //click events on each individual td
-  var size = Object.keys(places).length;
-  for (var i = 1; i <= size; i++) {
-    console.log(places[i]);
-    this.onclick = function(){      console.log("I'm " + places[i]);
-  };
-}
-
-  
-// });
-
-//if the user clicks on a section, a char should appear
+  //if the user clicks on a section, a char should appear
 
 
 });
