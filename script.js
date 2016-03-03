a5ac825a38846c30053e397a0aa53106f082d329
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-03-03 15:29:52 -0500

    test

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ $(".button").click(function(){
 // everytime a player clicks on a <td> an X or an O shows in the cell.
 console.log("I was clicked!");
   // How can tell which cell was clicked on?
-    var playerOne = X;
-    var playerTwo = O;
+  var playerOne = X;
+  var playerTwo = O;
+  var symbol = ["X","O"];
+  var id = [1, 2];
+  var player = function (id, symbol){
+    this.symbol = symbol;
+    this.id = id;
+  }
+
+
+
   // How can we determine whats in that cell?
 
   // if the value of the cell is not what we want?
