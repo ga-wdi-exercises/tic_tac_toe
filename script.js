017f0f7f199af419a35e1832d55855d7c4a0b041
Kimberly Cabbagestalk <kimcabb@gmail.com>
2016-03-03 13:28:01 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-$(document).ready function(){}
-console.log("The script is loaded!");
+//construct a 3x3 board, use for loop
+var rows = 3;
+var columns = 3;
+var $row = $("<div />", {
+    class: 'row'
+});
+var $cell = $("<div />", {
+    class: 'cell'
+});
+
+$(document).ready(function () {
+    //add columns to row
+    for (var i = 0; i < columns; i++) {
+        $row.append($cell.clone());
+    }
+    //clone the temp row object with the columns to the board
+    for (var i = 0; i < rows; i++) {
+        $("#board").append($row.clone());
+    }
+});
