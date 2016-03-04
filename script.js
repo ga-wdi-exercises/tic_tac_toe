97513f960d676407693b286b7c85075abe97f169
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-03-03 22:29:38 -0500

    changes game logic to list of arrays

--- script.js
+++ script.js
@@ -5,38 +5,52 @@ $(document).ready(function() {
   var winner = $("#winner");
   var board = $("#board");
   var clickCounter = 0;
-  var xScore = 1;
-  var oScore = 1;
+  var xScore = [[]];
+  var oScore = [[]];
+  var triples = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
+    ["1", "4", "7"],
+    ["2", "5", "8"],
+    ["3", "6", "9"],
+    ["1", "5", "9"],
+    ["7", "8", "9"]
+  ];
 
   var game = {
     play: function(){
       clickCounter++;
       if ((clickCounter % 2 === 0) && ($(this).hasClass("clicked")) === false){
         $(this).html("<img class='o' src='o.png'/>");
-        oScore = this.id * 7 * oScore;
-        console.log(oScore);
+        oScore.push.eq(0)(this.id);
       }
       else if ((clickCounter % 2 > 0) && ($(this).hasClass("clicked")) === false){
         $(this).html("<img class='x' src='x.png'/>");
-        xScore = this.id * 7 * xScore;
-        console.log(xScore);
+        xScore.push.eq(0)(this.id);
       }
       $(this).addClass("clicked");
       game.score();
     },
 
     score: function(){
-      if (xScore % 2058 === 0 || xScore % 172872 === 0 || xScore % 41160 === 0 || xScore % 15435 === 0 || xScore % 55566 === 0 || xScore % 27440 === 0 || xScore % 9604 === 0 || xScore % 36015 === 0){
-        winner.html("X wins!");
-        winner.css("backgroundColor", "red");
-        console.log("x score is " + xScore);
+      // if (xScore.length >= 3){
+      // // for (var i = 0; i <= xScore.length; i++) {
+      //   for (var j = 0; j < 3; j++)
+        if($.each(xScore, triples) === true) {
+          winner.html("X wins!");
+          winner.css("backgroundColor", "red");
       }
-      if (oScore % 2058 === 0 || oScore % 172872 === 0 || oScore % 41160 === 0 || oScore % 15435 === 0 || oScore % 55566 === 0 || oScore % 27440 === 0 || oScore % 9604 === 0 || oScore % 36015 === 0){
-        winner.html("O wins!");
-        winner.css("backgroundColor", "red");
-        console.log("y score is " + oScore);
+    if (oScore.length >= 3){
+      // for (var k = 0; k <= oScore.length; k++) {
+        // for (var l = 0; l < 3; l++)
+        if($.each(oScore, triples) === true) {
+          winner.html("O wins!");
+          winner.css("backgroundColor", "red");
+        }
       }
-  },
+
+},
 
     resetGame: function() {
       square.html("");
