24adbcfcbae0a6387d48897582f7dc2f62c97544
Kimberly Cabbagestalk <kimcabb@gmail.com>
2016-03-03 20:20:02 -0500

    updated js

--- script.js
+++ script.js
@@ -8,13 +8,49 @@ var $cell = $("<div />", {
     class: 'cell'
 });
 
-$(document).ready(function () {
-    //add columns to row
-    for (var i = 0; i < columns; i++) {
-        $row.append($cell.clone());
-    }
-    //clone the temp row object with the columns to the board
-    for (var i = 0; i < rows; i++) {
-        $("#board").append($row.clone());
-    }
+// Create grid **********************
+$(document).ready(function() {
+  //add columns to row
+  for (var i = 0; i < columns; i++){
+    $row.append($cell.clone());
+  }
+
+  //clone the temp row object with the columns to the board
+  for (var i = 0; i < rows; i++){
+    $("#board").append($row.clone());
+  }
+//************************************
+
+/*
+ //when player clicks on a cell show X
+  $(".cell").click(function(){
+    $(this.show());
+  });
+*/
+
+  //something easier...
+    //when player clicks on a cell the color changes
+      $(".cell").on("click", function(){
+        $(this).css("background-color", "white");
+      });  //this works!
+
+//when player clicks cell a second time, show O
+//keep track of clicks
+//no third clicks
+//reset button
 });
+
+/*
+//when player clicks on a cell show X
+$(".cell").click(function(){
+  $(this.show());
+});
+*/
+
+//something easier...
+//when player clicks on a cell the color changes
+
+//when player clicks cell a second time, show O
+//keep track of clicks
+//no third clicks
+//reset button
