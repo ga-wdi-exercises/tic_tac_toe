2cf580cc1aae677db3b26d00f3df2de40ec2eca5
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-03 23:47:17 -0500

    more additions

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-// $(document).ready(function(){
-//
-//   var player;
-//   var messages = $(".messages");
-//   var turn = $(".turn");
-//
-//   getNextPlayer(turn, player);
-//
-// //click a cell
-// $("td").click(function(){
-//   td = $(this);
-//   var sign = chooseSign(td);
-//     if(!sign) {
-//       var pattern = patternForPlayer(player);
-//       chooseSign(td, pattern);
-///function runGame
+// $(document).ready(function()
+
 document.onload(runGame();)
 
 function runGame(){
