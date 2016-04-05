103430cd7870cae5ef7bb15203df02afbd84657b
Zaneh Williams <zanehmw@gmail.com>
2016-04-05 11:41:22 -0400

    added clicks

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("happy")
+$(document).ready(function() {
+  var game = new Game()
+})
+
+function Game() {
+  var self = this;
+  this.click = 0
+  this.squares = $(".square")
+  this.squares.html("")
+  this.squares.on("click", function(){
+    console.log("hello")
+    console.log(self.click)
+      self.click++;
+    if (self.click % 2 !== 0) {
+      $(this).css("backgroundColor", "blue")
+    }
+    else if (self.click % 2 == 0) {
+      $(this).css("backgroundColor", "red")
+    }
+  })
+
+};
