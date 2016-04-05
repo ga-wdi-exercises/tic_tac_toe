8d8c3c4f72adb9823e3636b24ab8e66714bda76a
Leland Jordon <lelandjordon@mac.com>
2016-04-05 16:44:54 -0400

    Still working on checkWinner bug.

--- script.js
+++ script.js
@@ -43,87 +43,90 @@ boardReset();
 
 
 checkWinner = function(){
+  console.log("checkwinner");
   // check horizontal 1-3
-if (($("#block1").html() == $("#block2").html()) && ($("#block2").html() == $("#block3").html())){
-  if ($("#block1").html() == "T"){
-    return;
-  }
-  else {
-    console.log("line 48");
-   alert($("#block1").html() + " wins");
-  }
+    if (($("#block1").html() == $("#block2").html()) && ($("#block2").html() == $("#block3").html())){
+      if ($("#block1").html() == "T"){
+        return;
+      }
+      else {
+        console.log("line 48");
+       alert($("#block1").html() + " wins 53");
+      }
+     }
+     // check vertical 1-7
+    if (($("#block1").html() == $("#block4").html()) && ($("#block4").html() == $("#block7").html())){
+      if ($("#block1").html() == "T"){
+        return;
+      }
+      else{
+       alert($("#block1").html() + " wins 62");
+      }
+    }
+    
+    //check horizontal 4-6
+     if (($("#block4").html() == $("#block5").html()) && ($("#block5").html() == $("#block6").html())){
+       console.log($("#block4").html() + " wins");
+       if ($("#block4").html() == "T"){
+         return;
+       }
+       else{
+         console.log("line 73");
+        alert($("#block4").html() + " wins");
+       }
+     }
+     //check horizontal 7-9
+    if (($("#block7").html() == $("#block8").html()) && ($("#block8").html() == $("#block9").html())){
+      //  console.log($("#block7").html() + " wins");
+       if ($("#block7").html() == "T"){
+         return;
+       }
+       else{
+        alert($("#block7").html() + " wins");
+       }
+     }
+     // check vertical 2-5-8
+    if (($("#block2").html() == $("#block5").html()) && ($("#block5").html() == $("#block8").html())){
+       console.log($("#block2").html() + " wins");
+       if ($("#block2").html() == "T"){
+         return;
+       }
+       else{
+        alert($("#block2").html() + " wins 93");
+       }
+     }
+     // check vertical 3-6-9
+    if (($("#block3").html() == $("#block6").html()) && ($("#block6").html() == $("#block9").html())){
+       console.log($("#block3").html() + " wins");
+       if ($("#block3").html() == "T"){
+         console.log("line 94")
+         return;
+       }
+       else{
+        alert($("#block3").html() + " wins 104");
+       }
+     }
+     // check diagonal 1-5-9
+    if (($("#block1").html() == $("#block5").html()) && ($("#block5").html() == $("#block9").html())){
+       console.log($("#block1").html() + " wins");
+       if ($("#block1").html() == "T"){
+         return;
+       }
+       else{
+        alert($("#block1").html() + " wins");
+       }
+     }
+     // check diagonal 3-5-7
+    if (($("#block3").html() == $("#block5").html()) && ($("#block5").html() == $("#block7").html())){
+       console.log($("#block3").html() + " wins");
+       if ($("#block3").html() == "T"){
+         return;
+       }
+       else{
+        alert($("#block3").html() + " wins");
+       }
  }
- // check vertical 1-7
-if (($("#block1").html() == $("#block4").html()) && ($("#block4").html() == $("#block7").html())){
-  if ($("#block1").html() == "T"){
-    return;
-  }
-  else{
-   alert($("#block1").html() + " wins");
-  }
-}
-//check horizontal 4-6
-if (($("#block4").html() == $("#block5").html()) && ($("#block5").html() == $("#block6").html())){
-   console.log($("#block4").html() + " wins");
-   if ($("#block4").html() == "T"){
-     return;
-   }
-   else{
-     console.log("line 67");
-    alert($("#block4").html() + " wins");
-   }
- }
- //check horizontal 7-9
-if (($("#block7").html() == $("#block8").html()) && ($("#block8").html() == $("#block9").html())){
-   console.log($("#block7").html() + " wins");
-   if ($("#block7").html() == "T"){
-     return;
-   }
-   else{
-    alert($("#block7").html() + " wins");
-   }
- }
- // check vertical 2-5-8
-if (($("#block2").html() == $("#block5").html()) && ($("#block5").html() == $("#block8").html())){
-   console.log($("#block2").html() + " wins");
-   if ($("#block2").html() == "T"){
-     return;
-   }
-   else{
-    alert($("#block2").html() + " wins 88");
-   }
- }
- // check vertical 3-6-9
-if (($("#block3").html() == $("#block6").html()) && ($("#block6").html() == $("#block9").html())){
-   console.log($("#block3").html() + " wins");
-   if ($("#block3").html() == "T"){
-     console.log("line 94")
-     return;
-   }
-   else{
-    alert($("#block3").html() + " wins");
-   }
- }
- // check diagonal 1-5-9
-if (($("#block1").html() == $("#block5").html()) && ($("#block5").html() == $("#block9").html())){
-   console.log($("#block1").html() + " wins");
-   if ($("#block1").html() == "T"){
-     return;
-   }
-   else{
-    alert($("#block1").html() + " wins");
-   }
- }
- // check diagonal 3-5-7
-if (($("#block3").html() == $("#block5").html()) && ($("#block5").html() == $("#block7").html())){
-   console.log($("#block3").html() + " wins");
-   if ($("#block3").html() == "T"){
-     return;
-   }
-   else{
-    alert($("#block3").html() + " wins");
-   }
- }
+ console.log("exit checkWinner");
 };
 
 
