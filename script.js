2508a545bd316341790d2b83ae0f0e55d962c61a
Michael Nahum <USGrant63@gmail.com>
2016-04-05 18:05:57 -0400

    almost-bug free version

--- script.js
+++ script.js
@@ -3,30 +3,51 @@ var clicks = 0;
 function paint() {
     $(".square").on("click", function (){
       clicks++;
-      if(clicks % 2 == 0) {
+      if(clicks % 2 === 0) {
         $(this).addClass("player-two");
-        $("#turn").html("First Player Turn")
+        $("#turn").html("First Player Turn");
       } else {
         $(this).addClass("player-one");
-        $("#turn").html("Second Player Turn")
+        $("#turn").html("Second Player Turn");
       }
+      getWinner();
       $(this).off()
     });
   }
 
 function reset() {
-    clicks = 0;
-    $(".square").css("backgroundColor", "white");
+   clicks = 0;
+    $(".square").removeClass("player-one");
+    $(".square").removeClass("player-two");
     $("#turn").html("First Player Turn");
-    paint();
+   paint();
 }
 
-paint()
+paint();
 
 $("button").on("click", reset);
 
-var board = {
-  box1: 1, box2: 2, box3: 4,
-  box4: 8, box5: 16, box6: 32,
-  box7: 64, box8: 128, box9: 256
+
+row1  = [0, 1, 2];
+row2  = [3, 4, 5];
+row3  = [6, 7, 8];
+col1  = [0, 3, 6];
+col2  = [1, 4, 7];
+col3  = [2, 5, 8];
+diag1 = [0, 4, 8];
+diag2 = [2, 4, 6];
+
+function getWinner(){
+  winningArrays = [row1, row2, row3, col1, col2, col3, diag1, diag2];
+  for (i = 0; i<winningArrays.length; i++){
+    currentRow = winningArrays[i];
+    if ($("#" + currentRow[0]).hasClass("player-one") &&  $("#" + currentRow[1]).hasClass("player-one") && $("#" + currentRow[2]).hasClass("player-one")){
+      alert("Player One has won! Player Two's failure disgraces the House of Atreus.");
+      reset();
+    }
+    else if ($("#" + currentRow[0]).hasClass("player-two") &&  $("#" + currentRow[1]).hasClass("player-two") && $("#" + currentRow[2]).hasClass("player-two")){
+      alert("Player Two has won! Player One's shame will echo through eternity.");
+      reset();
+    }
+  }
 }
