39e1903fc7ae4c2290710e4a1960e0cda4fa57b7
Matt <mvaden08@gmail.com>
2016-04-05 11:38:57 -0400

    updated JS

---
+++ script.js
@@ -0,0 +1,41 @@
+
+// $('#button').click(function () {
+//    if($('#mydiv').text()=="ko")
+//        $('#mydiv').text('ok');
+//    else
+//        $('#mydiv').text('ko');
+// });
+
+// $('#someElement').toggle(function(){
+//    $(this).text('text1');
+//   },
+//  function(){
+//    $(this).text('text2');
+//  }
+// );
+
+// First variable.
+var clicks = 0
+
+// Second variable.
+var square = ["square1", "square2", "square3", "square4", "square5", "square6", "square7", "square8", "square9"]
+
+var turn = 1
+var turnCounter = $("#button")
+
+$("square").toggle(function())
+document.querySelectorAll(".square")[0]
+.square1.addeventlistener("click", playerTurn)
+
+
+turnCounter.addEventListerner("click", updateTurnCount)
+
+function updateTurnCount () {
+   turn = turn++
+   turnCounter.textContent = ("It is turn"+""+turn);
+}
+// .on("click", turnCounter++)
+
+function playerTurn()
+
+function turn()
