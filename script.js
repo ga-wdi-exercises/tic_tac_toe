694586f42f7f52cc6a9c6b2631532ae010ba7f5f
Habby Whyte <bahybaj@hotmail.com>
2016-04-05 13:31:17 -0400

    made variable global

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 $("document").ready(function(){
 
-  // function display(){
-  //   console.log(this);
-  //   //if odd clicks, an X will appear
-  //   // clicks++
-  //   // if (clicks % 2== 0)
-  //   //set clicked div with an attribute
-  // // this.text("O");
-  // }
 
+
+var click = 0
   // for (var i = 0; i < 9; i++){
   $(".square").each(function(){
-    var click = 0
+
     $(this).on("click",(function(){
       click++
       console.log("it clicked");
@@ -29,31 +23,3 @@ $("document").ready(function(){
 
 
 })
-
-//when you click a box an "X" or "O" appears
-//when you click on any first box an X appears
-//first click(odd) = X
-//second click(even) = O
-//if player clicks even number of times = X
-//if player clicks odd number of times = O
-
-
-// function clickSquare (){
-//   if (clicks % 2 ===0){
-//   clicks++
-//
-//
-// }
-// }
-// var squares[{
-//     el : $("#a");
-//   },
-//   var square2 = $("#b");
-//   var square3 = $("#c");
-//   var square4 = $("#d");
-//   var square5 = $("#e");
-//   var square6 = $("#f");
-//   var square7 = $("#g");
-//   var square8 = $("#h");
-//   var square9 = $("#i");
-// }]
