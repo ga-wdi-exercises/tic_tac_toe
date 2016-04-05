56686fac42c1c3559f73fbc9da3fb34486373231
Leland Jordon <lelandjordon@mac.com>
2016-04-05 15:30:28 -0400

    Fixed checkWinner function; added jquery script file because jquery.com was down.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ clickCounter = 0;
 playBlock = function(){
   $('.block').click(function(){
       var clickBlock = this.id;
-      console.log(clickBlock);
+      // console.log(clickBlock);
       if ($(this).hasClass("clicked")){
         console.log("nope");
         return;
@@ -39,32 +39,84 @@ boardReset();
 
 
 checkWinner = function(){
+  // check horizontal 1-3
 if (($("#block1").html() == $("#block2").html()) && ($("#block2").html() == $("#block3").html())){
-   console.log($("#block1").html() + " wins");
- };
-if (($("#block1").html() == $("#block4").html()) && ($("#block1").html() == $("#block7").html())){
-   console.log($("#block1").html() + " wins");
- };
-if (($("#block4").html() == $("#block5").html()) && ($("#block4").html() == $("#block6").html())){
+  if ($("#block1").html() == "T"){
+    return;
+  }
+  else {
+    console.log("line 48");
+   alert($("#block1").html() + " wins");
+  }
+ }
+ // check vertical 1-7
+if (($("#block1").html() == $("#block4").html()) && ($("#block4").html() == $("#block7").html())){
+  if ($("#block1").html() == "T"){
+    return;
+  }
+  else{
+   alert($("#block1").html() + " wins 58");
+  }
+}
+//check horizontal 4-6
+if (($("#block4").html() == $("#block5").html()) && ($("#block5").html() == $("#block6").html())){
    console.log($("#block4").html() + " wins");
- };
-if (($("#block7").html() == $("#block8").html()) && ($("#block7").html() == $("#block9").html())){
+   if ($("#block4").html() == "T"){
+     return;
+   }
+   else{
+     console.log("line 67");
+    alert($("#block4").html() + " wins 69");
+   }
+ }
+ //check horizontal 7-9
+if (($("#block7").html() == $("#block8").html()) && ($("#block8").html() == $("#block9").html())){
    console.log($("#block7").html() + " wins");
- };
-if (($("#block2").html() == $("#block5").html()) && ($("#block2").html() == $("#block8").html())){
+   if ($("#block7").html() == "T"){
+     return;
+   }
+   else{
+    alert($("#block7").html() + " wins 79");
+   }
+ }
+if (($("#block2").html() == $("#block5").html()) && ($("#block5").html() == $("#block8").html())){
    console.log($("#block2").html() + " wins");
- };
-if (($("#block3").html() == $("#block6").html()) && ($("#block3").html() == $("#block9").html())){
+   if ($("#block2").html() == "T"){
+     return;
+   }
+   else{
+    alert($("#block2").html() + " wins 88");
+   }
+ }
+if (($("#block3").html() == $("#block6").html()) && ($("#block6").html() == $("#block9").html())){
    console.log($("#block3").html() + " wins");
- };
-if (($("#block1").html() == $("#block5").html()) && ($("#block1").html() == $("#block9").html())){
+   if ($("#block3").html() == "T"){
+     console.log("line 94")
+     return;
+   }
+   else{
+    alert($("#block3").html() + " wins 97");
+   }
+ }
+if (($("#block1").html() == $("#block5").html()) && ($("#block5").html() == $("#block9").html())){
    console.log($("#block1").html() + " wins");
- };
-if (($("#block3").html() == $("#block5").html()) && ($("#block1").html() == $("#block7").html())){
+   if ($("#block1").html() == "T"){
+     return;
+   }
+   else{
+    alert($("#block1").html() + " wins 106");
+   }
+ }
+if (($("#block3").html() == $("#block5").html()) && ($("#block5").html() == $("#block7").html())){
    console.log($("#block3").html() + " wins");
- };
+   if ($("#block3").html() == "T"){
+     return;
+   }
+   else{
+    alert($("#block3").html() + " wins 115");
+   }
+ }
 };
-
 
 
 
