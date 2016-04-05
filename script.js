23ef8d875f593294aa966f535b226218150bc1e3
Matt <mvaden08@gmail.com>
2016-04-05 16:06:30 -0400

    updated index and script (Matthew and Antony).

--- script.js
+++ script.js
@@ -1,41 +1,59 @@
+var click = 0;
 
-// $('#button').click(function () {
-//    if($('#mydiv').text()=="ko")
-//        $('#mydiv').text('ok');
-//    else
-//        $('#mydiv').text('ko');
-// });
+function updateSquare() {
+ console.log("jk");
+ click = click + 1;
+ console.log(click);
+ if (click%2 !==0) {
+   square1.textContent= "X";
+   square2.textContent= "X";
+   square3.textContent= "X";
+   square4.textContent= "X";
+   square5.textContent= "X";
+   square6.textContent= "X";
+   square7.textContent= "X";
+   square8.textContent= "X";
+   square9.textContent= "X";
+ } else {
+   square1.textContent= "O";
+   square2.textContent= "O";
+   square3.textContent= "O";
+   square4.textContent= "O";
+   square5.textContent= "O";
+   square6.textContent= "O";
+   square7.textContent= "O";
+   square8.textContent= "O";
+   square9.textContent= "O";
+ }
+}
 
-// $('#someElement').toggle(function(){
-//    $(this).text('text1');
-//   },
-//  function(){
-//    $(this).text('text2');
-//  }
-// );
+$( document ).ready(function() {
+ console.log("Pizza");
+ var square1 = $("#square1");
+ square1.on("click", updateSquare);
+});
 
-// First variable.
-var clicks = 0
+reset.on("click", resetBoard);
+   console.log("reset here.");
+function resetBoard() {
+   square1.textContent = "";
+}
 
 // Second variable.
-var square = ["square1", "square2", "square3", "square4", "square5", "square6", "square7", "square8", "square9"]
-
-var turn = 1
-var turnCounter = $("#button")
-
-$("square").toggle(function())
-document.querySelectorAll(".square")[0]
-.square1.addeventlistener("click", playerTurn)
-
-
-turnCounter.addEventListerner("click", updateTurnCount)
-
-function updateTurnCount () {
-   turn = turn++
-   turnCounter.textContent = ("It is turn"+""+turn);
-}
-// .on("click", turnCounter++)
-
-function playerTurn()
-
-function turn()
+var square =
+["square1",
+"square2",
+"square3",
+"square4",
+"square5",
+"square6",
+"square7",
+"square8",
+"square9"];
+// initial values.
+// square = null;
+// Turn variable.
+// var turn = 1;
+// Turn countervairable.
+var turnCounter = $("#button");
+var reset = $("#reset");
